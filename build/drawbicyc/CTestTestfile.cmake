bcb6918ca7674f49b1b97081f9920b0f0e1d4cfe
pdziekan <pdziekanp@gmail.com>
2016-11-02 15:58:32 +0100

    remove build tracking

--- build/drawbicyc/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/plgdziekan/code/bicycles/drawbicyc
-# Build directory: /home/plgdziekan/code/bicycles/build/drawbicyc
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
