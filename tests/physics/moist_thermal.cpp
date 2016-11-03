379fd1c767fdb34983c00727db0522ea9c884c07
pdziekan <pdziekanp@gmail.com>
2016-11-03 14:50:31 +0100

    moist thermal test, run also with SDs

--- tests/physics/moist_thermal.cpp
+++ tests/physics/moist_thermal.cpp
@@ -1,5 +1,10 @@
+// Moist thermal comparison in setup from Clark Grabowski 1999 JAS
+// only condensation and evaporation
+// tested both for blk_1m and lgrngn microphysics
+// second one takes long...
+
 #include <cstdlib> // system()
-#include <unordered_set>
+#include <unordered_map>
 #include <string>
 #include <sstream> // std::ostringstream
 
@@ -8,7 +13,7 @@
 #include "../../drawbicyc/src/common_filters.hpp"
 
 using std::ostringstream;
-using std::unordered_set;
+using std::unordered_map;
 using std::string;
 
 int main(int ac, char** av)
@@ -18,16 +23,30 @@ int main(int ac, char** av)
   string outdir;
   string opts_common = 
     "--outfreq=60 --nt=600 --spinup=1000 --dt=1 --nx=181 --nz=121 --th_src=false --uv_src=false --rv_src=false --w_src=true --adv_serial=true";
-  unordered_set<string> opts_micro({
-//    "--async=false --micro=lgrngn --outdir=out_lgrngn --backend=serial --sd_conc=8 --z_rlx_sclr=100 --unit_test=true",
-    "--micro=blk_1m --outdir=tmp_out --cond=true --cevp=true --revp=false --conv=false --accr=false --sedi=false"  
+  unordered_map<string, string> opts_micro({
+    {"lgrngn", "--micro=lgrngn --outdir=tmp_out --cond=true --adve=true --sedi=false --coal=false --backend=OpenMP --sd_conc=16"},
+    {"blk_1m", "--micro=blk_1m --outdir=tmp_out --cond=true --cevp=true --revp=false --conv=false --accr=false --sedi=false"}
   });
+
+  // expected results
+  // center of mass of rc
+  unordered_map<string, std::array<float, 11>> data_com = {
+    {"blk_1m", {{0., 0., 915.282, 1006.67, 1101.82, 1183.7, 1245.3, 1292.47, 1326.17, 1340.71, 1346.41}}},
+    {"lgrngn", {{0, 0, 914.761, 1006.64, 1100.94, 1179.43, 1235.54, 1282.71, 1337.99, 1370.28, 1344.92}}}
+  };
+  // average rc
+  unordered_map<string, std::array<float, 11>> data_avg = {
+    {"blk_1m", {{0, 1.3249e-05, 0.00011178, 0.000275819, 0.000421079, 0.000552953, 0.000621904, 0.000583853, 0.00051231, 0.000448865, 0.000410936}}},
+    {"lgrngn", {{0, 0,         0.000101064, 0.000268452, 0.000427039, 0.000546621, 0.000570856, 0.00055723, 0.000447908, 0.000374359, 0.000261464}}}
+  };
+  // relative precision
+  unordered_map<string, double> eps = { {"blk_1m", 1e-5}, {"lgrngn", 5e-2}};
 
   for (auto &opts_m : opts_micro)
   {
     // run the simulation
     ostringstream cmd;
-    cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m;
+    cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m.second;
     notice_macro("about to call: " << cmd.str())
 
     if (EXIT_SUCCESS != system(cmd.str().c_str()))
@@ -35,15 +54,12 @@ int main(int ac, char** av)
  
     // instantiate plotter to read in output
     using Plotter_t = PlotterMicro_t<2>;
-    Plotter_t plotter("tmp_out", "blk_1m");
+    Plotter_t plotter("tmp_out", opts_m.first);
     auto& n = plotter.map;
-    float data_com[] = {0., 0., 915.282, 1006.67, 1101.82, 1183.7, 1245.3, 1292.47, 1326.17, 1340.71, 1346.41};
-    float data_avg[] = { 0, 1.3249e-05, 0.00011178, 0.000275819, 0.000421079, 0.000552953, 0.000621904, 0.000583853, 0.00051231, 0.000448865, 0.000410936};
 
     blitz::Array<float, 1> result_com(n["t"]);
     blitz::Array<float, 1> result_avg(n["t"]);
     blitz::Array<double, 1> rel_err(n["t"]);
-    double eps = 1e-5; // relative precision; mostly due to data_ number of digits
 
     // read statistics
     for (int at = 0; at < n["t"]; ++at)
@@ -77,18 +93,18 @@ int main(int ac, char** av)
     }
 
     // compare center of mass
-    blitz::Array<float, 1> expected_result(data_com, 11, blitz::neverDeleteData);
+    blitz::Array<float, 1> expected_result(data_com[opts_m.first].data(), 11, blitz::neverDeleteData);
     rel_err = where(expected_result > 0, abs(result_com - expected_result) / expected_result, 0);
-    if(any(rel_err > eps))
+    if(any(rel_err > eps[opts_m.first]))
     {
       std::cerr << rel_err;
       error_macro("cloud droplets center of mass discrepancy");
     }
 
     // compare avg rc
-    expected_result = blitz::Array<float, 1>(data_avg, 11, blitz::neverDeleteData);
+    expected_result = blitz::Array<float, 1>(data_avg[opts_m.first].data(), 11, blitz::neverDeleteData);
     rel_err = where(expected_result > 0, abs(result_avg - expected_result) / expected_result, 0);
-    if(any(rel_err > eps))
+    if(any(rel_err > eps[opts_m.first]))
     {
       std::cerr << rel_err;
       error_macro("average cloud water mixing ratio discrepancy");
