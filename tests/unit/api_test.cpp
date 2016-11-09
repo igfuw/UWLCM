eb699c6bc2e3476afbc79ed2431a18b40e642011
pdziekan <pdziekanp@gmail.com>
2016-11-09 15:09:22 +0100

    skip 3d thermal tests

--- tests/unit/api_test.cpp
+++ tests/unit/api_test.cpp
@@ -1,12 +1,12 @@
 #include <cstdlib> // system()
-#include <unordered_set>
+#include <vector>
 #include <string>
 #include <sstream> // std::ostringstream
 
 #include "../common.hpp"
 
 using std::ostringstream;
-using std::unordered_set;
+using std::vector;
 using std::string;
 
 int main(int ac, char** av)
@@ -16,15 +16,15 @@ int main(int ac, char** av)
   string outdir;
   string opts_common = 
     "--outfreq=1000 --nt=2 --spinup=1 --dt=0.1 --serial=true"; // dt=1 caused blk1m dycoms to freeze on pressure solver
-  unordered_set<string> opts_dim({
+  vector<string> opts_dim({
     "--nx=4 --ny=4 --nz=4",
     "--nx=4 --nz=4"
   });
-  unordered_set<string> opts_micro({
+  vector<string> opts_micro({
     "--async=false --micro=lgrngn --outdir=out_lgrngn --backend=serial --sd_conc=8 --z_rlx_sclr=100 --unit_test=true",
     "--micro=blk_1m --outdir=out_blk_1m"  
   });
-  unordered_set<string> opts_case({
+  vector<string> opts_case({
     "--case=dry_thermal --cond=0 --coal=0",
     "--case=moist_thermal",
     "--case=dycoms"
@@ -34,6 +34,11 @@ int main(int ac, char** av)
     for (auto &opts_m : opts_micro)
       for (auto &opts_c : opts_case)
       {
+        if((opts_c ==  opts_case[0] || opts_c == opts_case[1]) && opts_d == opts_dim[0])
+        {
+          std::cout << "skipping 3d thermal tests" << std::endl;
+          continue; 
+        }
         ostringstream cmd;
         cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m << " " << opts_d << " " << opts_c;
         notice_macro("about to call: " << cmd.str())
