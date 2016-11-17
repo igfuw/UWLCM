12cd5f448e73a5d8411a31273acd93476dcd4b99
pdziekan <pdziekanp@gmail.com>
2016-11-17 11:20:08 +0100

    cleanup

--- drawbicyc/gnuplot.hpp
+++ drawbicyc/gnuplot.hpp
@@ -57,14 +57,3 @@ void init(
   gp << "set grid\n";
   gp << "set multiplot layout " << ny << "," << nx << "\n";
 }
-
-template <class data_t>
-void plot(Gnuplot &gp, const data_t &data)
-{
-  blitz::Array<float, 2> tmp(data);
-
-  gp << "set xrange [0:" << tmp.extent(0)-1 << "]\n";
-  gp << "set yrange [0:" << tmp.extent(1)-1 << "]\n";
-  gp << "splot '-' binary" << gp.binfmt(tmp.transpose(blitz::secondDim, blitz::firstDim)) << " scan=yx origin=(0,0,0) with image failsafe notitle\n";
-  gp.sendBinary(tmp);
-}
