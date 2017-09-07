e61c100cb6ca6909b0cfa06175a18c901dfe88e4
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2017-09-07 11:50:08 +0200

    mt test: more loops, less repetitions

--- tests/physics/moist_thermal.cpp
+++ tests/physics/moist_thermal.cpp
@@ -32,6 +32,12 @@ bool errcheck(barr1d result, barr1d expected_result, barr1d epsilon)
     return 0;
 }
 
+struct test_data
+{
+  unordered_map<string, std::array<double, 11>> expected,
+                                                epsilon;
+};
+
 int main(int ac, char** av)
 {
   if (ac != 2) error_macro("expecting one argument - CMAKE_BINARY_DIR");
@@ -41,9 +47,56 @@ int main(int ac, char** av)
     "--outfreq=60 --nt=600 --spinup=1000 --dt=1 --nx=181 --nz=121 --case=moist_thermal";
   unordered_map<string, string> opts_micro({
     {"blk_1m", "--micro=blk_1m --outdir=tmp_out_blk_1m --cond=true --cevp=true --revp=false --conv=false --accr=false --sedi=false"},
-    {"lgrngn", "--micro=lgrngn --outdir=tmp_out_lgrngn --cond=true --adve=true --sedi=false --coal=false --backend=OpenMP --sd_conc=32"}
+    {"lgrngn", "--micro=lgrngn --outdir=tmp_out_lgrngn --cond=true --adve=true --sedi=false --coal=false --backend=OpenMP --sd_conc=16"}
   });
 
+  // container for the expecged result and the epslion (req precision) for each tested statistic
+  unordered_map<string, test_data> tests;
+  // populate it - height of the center of mass
+  tests["com_z"] = {
+    // expected values map
+    {
+//    {"blk_1m", {{0., 0.,      915.282, 1006.67, 1101.82, 1183.7,  1245.3,  1292.47, 1326.17, 1340.71, 1346.41}}}, // old values from before the twomey SD bubble paper
+      {"blk_1m", {{0., 841.758, 917.667, 1022.48, 1134.36, 1232.02, 1308.47, 1364.44, 1412.22, 1476.16, 1521.43}}},  // new values, i.e. for env profs from the twomey SD paper and for abs instead of iga&fct
+      {"lgrngn", {{0, 0, 914.761, 1006.64, 1100.94, 1179.43, 1235.54, 1282.71, 1337.99, 1370.28, 1344.92}}} // old values are still good, because of the large error allowed here?
+    },
+    // epsilons map
+    {
+      {"blk_1m", {{1e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 5e-2, 2e-1}} },  // why larger near the end?
+      {"lgrngn", {{5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 10e-2, 15e-2, 5e-1, 5e-1, 1.5}} }   // during evaporation we get large fluctuations
+    }
+  };
+  // average mass mixing ratio of activated dropletes in cloudy cells
+  tests["rc_avg"] = {
+    // expected values map
+    {
+  //    {"blk_1m", {{0, 1.3249e-02, 0.111779, 0.275816, 0.421085, 0.552938, 0.621531, 0.585304, 0.513864, 0.440379, 0.406745}}}, // old values from before the twomey SD bubble paper (ammonium sulphate aerosol + old env_profs + iga&fct)
+      {"blk_1m", {{0, 0, 0.185381, 0.357674, 0.518615, 0.635203, 0.707202, 0.733656, 0.686596, 0.556995, 0.425793 }}}, // new values, i.e. for env profs from the twomey SD paper and for abs instead of iga&fct
+  //    {"lgrngn", {{0, 0, 9.43111e-02, 0.258181, 0.41635, 0.533337, 0.590126, 0.575933, 0.496402, 0.391189, 0.295669}}} // old values from before the twomey SD bubble paper (ammonium sulphate aerosol + old env_profs + iga&fct)
+      {"lgrngn", {{ 0, 0, 0.179877, 0.374551, 0.552171, 0.704754, 0.790675, 0.791144, 0.73452, 0.648194, 0.548742}}}  // values for NaCl and env_profs used in the twomey SD bubble paper
+    },
+    // epsilons map
+    {
+      {"blk_1m", {{1e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 5e-2, 2e-1}} },  // why larger near the end?
+      {"lgrngn", {{5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 10e-2, 15e-2, 5e-1, 5e-1, 1.5}} }   // during evaporation we get large fluctuations
+    }
+  };
+  // standard deviation of the mass mixing ratio of activated droplets in cloudy cells
+  tests["rc_std_dev"] = {
+    // expected values map
+    {
+      {"blk_1m", {{0, 0, 0.0189641, 0.0669555, 0.137429, 0.210689, 0.266551, 0.30766, 0.333871, 0.310118, 0.232963}}},
+      {"lgrngn", {{0, 0, 0.0551891, 0.116698, 0.202856, 0.290047, 0.375072, 0.442025, 0.485523, 0.525853, 0.523559}}} // sd=16
+  //    {"lgrngn", {{0, 0, 0.0464611, 0.105821, 0.164722, 0.234542, 0.310864, 0.375448, 0.434617, 0.492958, 0.512137}}} // sd=32
+    },
+    // epsilons map
+    {
+      {"blk_1m", {{1e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 5e-2, 2e-1}} },  // why larger near the end?
+      {"lgrngn", {{5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 10e-2, 15e-2, 5e-1, 5e-1, 1.5}} }   // during evaporation we get large fluctuations
+    }
+  };
+
+/*
   // expected results
   // center of mass of rc
   unordered_map<string, std::array<double, 11>> data_com = {
@@ -69,8 +122,11 @@ int main(int ac, char** av)
     {"blk_1m", {{1e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 2e-2, 5e-2, 2e-1}} },  // why larger near the end?
     {"lgrngn", {{5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 5e-2, 10e-2, 15e-2, 5e-1, 5e-1, 1.5}} }   // during evaporation we get large fluctuations
   };
+*/
   // out dir
   unordered_map<string, string> tmp_out = { {"blk_1m", "tmp_out_blk_1m"}, {"lgrngn", "tmp_out_lgrngn"}};
+
+  // flag indicating if there was an error in any statistic
   bool err_flag = 0;
 
   for (auto &opts_m : opts_micro)
@@ -91,6 +147,29 @@ int main(int ac, char** av)
     blitz::Array<double, 1> result(n["t"]);
 
     // compare statistics
+    for(auto &test: tests)
+    {
+      // calculate the statistic from the simulation at each output step
+      for (int at = 0; at < n["t"]; ++at)
+      {
+        if(test.first == "com_z")
+          result(at) = plotter.act_com_z_timestep(at * 60); 
+        else if(test.first == "rc_avg")
+          result(at) = (plotter.cloud_ract_stats_timestep(at * 60)).first;
+        else if(test.first == "rc_std_dev")
+          result(at) = (plotter.cloud_ract_stats_timestep(at * 60)).second;
+      }
+
+      // output the result
+      cout << test.first << " : " << result;
+      // read in expected result and the epsilon
+      blitz::Array<double, 1> expected_result(test.second.expected[opts_m.first].data(), 11, blitz::neverDeleteData);
+      blitz::Array<double, 1> epsilon(test.second.epsilon[opts_m.first].data(), 11, blitz::neverDeleteData);
+      // check if there is an agreement
+      err_flag = errcheck(result, expected_result, epsilon) || err_flag;
+    }
+
+    /*
     // height of the center of mass of cloud droplets
     for (int at = 0; at < n["t"]; ++at)
       result(at) = plotter.act_com_z_timestep(at * 60); 
@@ -118,6 +197,7 @@ int main(int ac, char** av)
     for (int at = 0; at < n["t"]; ++at)
     {
     }
+*/
   }
 
   if(err_flag)
