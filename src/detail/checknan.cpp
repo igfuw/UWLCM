bf555e0a1f1b09321c1eefe695ffeeed2ad2c7b5
pdziekan <pdziekanp@gmail.com>
2017-11-30 14:52:49 +0100

    CHEATING: turn negative rv to zero

--- src/detail/checknan.cpp
+++ src/detail/checknan.cpp
@@ -17,3 +17,10 @@
                                std::cout << arr;\
                                assert(0);}}
 #endif
+
+#define negtozero(arr, name) {if(min(arr) < 0.) {\
+                               std::cout << "A negative number detected in: " << name << std::endl;\
+                               std::cout << arr;\
+                               std::cout << "CHEATING: turning negative values to zeroes" << std::endl;\
+                               arr = where(arr < 0., 0., arr);\
+                               }}
