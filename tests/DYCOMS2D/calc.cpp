431e282b231545ef0ffd90b642daded0fc714a81
Anna Jaruga <ajaruga@caltech.edu>
2018-02-05 14:18:59 -0800

    revert changes to master part1

--- tests/DYCOMS2D/calc.cpp
+++ tests/DYCOMS2D/calc.cpp
@@ -37,11 +37,10 @@ int main(int ac, char** av)
   std::cout << "output directory: " << outdir << std::endl;
 */
   string opts_common = 
-    "--outfreq=600 --nt=7200 --spinup=3600 --nx=33 --nz=76 --dt=1 --relax_th_rv=false --piggy=0 --save_vel=1 --case=dycoms "; // DYCOMS: 128x300 ; 600 21600 3600
+    "--outfreq=600 --nt=7200 --spinup=3600 --nx=33 --nz=76 --dt=1 --relax_th_rv=false"; // DYCOMS: 128x300 ; 600 21600 3600
   set<string> opts_micro({
-    "--micro=blk_1m --outdir=out_blk_1m"//,
+//    "--micro=blk_1m --outdir=out_blk_1m"//,
 //    "--micro=blk_2m --outdir=out_blk_2m",
-/*
     "--adv_serial=false --async=true --micro=lgrngn --outdir=out_lgrngn --backend=CUDA --sd_conc=32 --sstp_cond=1 --z_rlx_sclr=100 --sstp_coal=1"  
     " --coal=false"
       " --out_wet=\""
@@ -53,7 +52,6 @@ int main(int ac, char** av)
 //        + bins_dry_str + // aerosol spectrum (dry)
 //
       "\""
-*/
   });
 
   for (auto &opts_m : opts_micro)
