a013e95be0ec04638ab28fe98646bdc31963a52e
pdziekan <pdziekanp@gmail.com>
2018-03-21 10:12:55 +0100

    negtozero: not to 0 but to 1e-10

--- src/detail/checknan.cpp
+++ src/detail/checknan.cpp
@@ -18,9 +18,10 @@
                                assert(0);}}
 #endif
 
+// actually not to zero, but to 1e-10 (we need rv>0 in libcloud and cond substepping numerical errors colud lead to rv<0 if we would set it here to 0)
 #define negtozero(arr, name) {if(min(arr) < 0.) {\
                                std::cout << "A negative number detected in: " << name << std::endl;\
                                std::cout << arr;\
                                std::cout << "CHEATING: turning negative values to zeroes" << std::endl;\
-                               arr = where(arr < 0., 0., arr);\
+                               arr = where(arr <= 0., 1e-10, arr);\
                                }}
