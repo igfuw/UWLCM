52151c0f5b0b94d44736d7fc21a177c6f334bd61
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-04-24 15:54:34 +0200

    blk_1m: also pass p_d_env to adj_cellwise

--- src/cases/DryThermalGMD2015.hpp
+++ src/cases/DryThermalGMD2015.hpp
@@ -89,7 +89,7 @@ namespace setup
 //        const quantity<si::dimensionless, real_t> p_theta = (quantity<si::mass_density, real_t>(rhod * si::kilograms / si::cubic_metres) * R_d<real_t>() * quantity<si::temperature, real_t>(th_e * si::kelvins))/si::pascals;
         const quantity<si::pressure, real_t> p_theta = (quantity<si::mass_density, real_t>(1. * si::kilograms / si::cubic_metres) * R_d<real_t>() * quantity<si::temperature, real_t>(300. * si::kelvins));
         /*const quantity<si::dimensionless, real_t>*/ real_t p = pow( real_t(p_theta / si::pascals) * pow(real_t(p_1000<real_t>() / si::pascals), R_d_over_c_pd<real_t>()), 1. / (R_d_over_c_pd<real_t>() + 1)) ;// * pow(p_1000<real_t>(), R_d_over_c_pd<real_t>()));//, 1. / (R_d_over_c_pd<real_t>() + 1)) / si::pascals;
-        p_e = p;
+        p_e = p; // total env pressure
         th_ref = 300;
       }
     };
