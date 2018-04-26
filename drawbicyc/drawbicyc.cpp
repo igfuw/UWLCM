2856948412cd7c1fbb411c0ea2c11e1137f14a13
pdziekan <pdziekanp@gmail.com>
2018-04-26 15:10:57 +0200

    various plotting imporvements

--- drawbicyc/drawbicyc.cpp
+++ drawbicyc/drawbicyc.cpp
@@ -56,18 +56,25 @@ int main(int argc, char** argv)
 
   if(NDims == 2)
   {
+/*
     if(flag_series)   plot_series(PlotterMicro_t<2>(h5, micro), Plots(type));
     if(flag_profiles) plot_profiles(PlotterMicro_t<2>(h5, micro), Plots(type));
     if(flag_fields)   plot_fields(PlotterMicro_t<2>(h5, micro), Plots(type));
     if(flag_qv_qc_2_6_10_min)   plot_qv_qc_2_6_10_min(PlotterMicro_t<2>(h5, micro), Plots(type));
+*/
   }
   else if(NDims == 3)
   {
+/*
     if(flag_series)   plot_series(PlotterMicro_t<3>(h5, micro), Plots(type));
     if(flag_profiles) plot_profiles(PlotterMicro_t<3>(h5, micro), Plots(type));
     if(flag_fields)   plot_fields(PlotterMicro_t<3>(h5, micro), Plots(type));
     if(flag_qv_qc_2_6_10_min)   plot_qv_qc_2_6_10_min(PlotterMicro_t<2>(h5, micro), Plots(type));
-    if(flag_lgrngn_spec)   plot_lgrngn_spec(PlotterMicro_t<3>(h5, "lgrngn"), Plots(type), 12);
+*/
+    if(flag_lgrngn_spec) {
+      plot_lgrngn_spec_positions(PlotterMicro_t<3>(h5, "lgrngn"), Plots(type), 12);
+      plot_lgrngn_spec(PlotterMicro_t<3>(h5, "lgrngn"), Plots(type), 12);
+    }
   }
   else
     assert(false && "need 2d or 3d input data");
