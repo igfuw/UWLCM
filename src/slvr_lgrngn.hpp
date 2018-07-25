72c043e1d92bf79153b61c2d655cfdbb1f328f34
pdziekan <pdziekanp@gmail.com>
2018-07-25 15:56:29 +0200

    lgrngn: sync in before rhs_apply, cond after rhs_apply

--- src/slvr_lgrngn.hpp
+++ src/slvr_lgrngn.hpp
@@ -403,100 +403,6 @@ class slvr_lgrngn : public slvr_common<ct_params_t>
       nancheck(rl, "rl after copying from diag_wet_mom(3)");
       rl = rl * 4./3. * 1000. * 3.14159; // get mixing ratio [kg/kg]
 
-      {
-        // temporarily Cx & Cz are multiplied by this->rhod ...
-        auto 
-          Cx = this->mem->GC[0](this->Cx_domain).reindex(this->zero).copy(),
-          Cy = this->mem->GC[1](this->Cy_domain).reindex(this->zero).copy(),
-          Cz = this->mem->GC[ix::w](this->Cz_domain).reindex(this->zero).copy(); 
-        nancheck(Cx, "Cx after copying from mpdata");
-        nancheck(Cy, "Cy after copying from mpdata");
-        nancheck(Cz, "Cz after copying from mpdata");
-
-        // ... and now dividing them by this->rhod (TODO: z=0 is located at k=1/2)
-        {
-          Cx.reindex(this->zero) /= (*params.rhod)(this->vert_idx);
-          Cy.reindex(this->zero) /= (*params.rhod)(this->vert_idx);
-          Cz.reindex(this->zero) /= (*params.rhod)(this->vert_idx); // TODO: should be interpolated, since theres a shift between positions of rhod and Cz
-        }
-
-        // running synchronous stuff
-        parent_t::tbeg = parent_t::clock::now();
-
-        // rv might be negative due to large negative RHS from SD fluctuations + large-scale subsidence?
-        // turn all negative rv into rv = 0... CHEATING
-        negtozero(this->mem->advectee(ix::rv), "rv before step sync");
-
-        nancheck(this->mem->advectee(ix::th), "th before step sync");
-        nancheck(this->mem->advectee(ix::rv), "rv before step sync");
-        negcheck(this->mem->advectee(ix::th), "th before step sync");
-        negcheck(this->mem->advectee(ix::rv), "rv before step sync");
-
-        using libcloudphxx::lgrngn::particles_t;
-        using libcloudphxx::lgrngn::CUDA;
-        using libcloudphxx::lgrngn::multi_CUDA;
-
-#if defined(STD_FUTURE_WORKS)
-        if (params.async)
-        {
-
-          prtcls->sync_in(
-            make_arrinfo(this->mem->advectee(ix::th)),
-            make_arrinfo(this->mem->advectee(ix::rv)),
-            libcloudphxx::lgrngn::arrinfo_t<real_t>(),
-            make_arrinfo(Cx),
-            this->n_dims == 2 ? libcloudphxx::lgrngn::arrinfo_t<real_t>() : make_arrinfo(Cy),
-            make_arrinfo(Cz)
-          );
-
-          assert(!ftr.valid());
-          if(params.backend == CUDA)
-            ftr = std::async(
-              std::launch::async, 
-              &particles_t<real_t, CUDA>::step_cond, 
-              dynamic_cast<particles_t<real_t, CUDA>*>(prtcls.get()),
-              params.cloudph_opts,
-              make_arrinfo(this->mem->advectee(ix::th)),
-              make_arrinfo(this->mem->advectee(ix::rv)),
-              std::map<enum libcloudphxx::lgrngn::chem_species_t, libcloudphxx::lgrngn::arrinfo_t<real_t> >()
-            );
-          else if(params.backend == multi_CUDA)
-            ftr = std::async(
-              std::launch::async, 
-              &particles_t<real_t, multi_CUDA>::step_cond, 
-              dynamic_cast<particles_t<real_t, multi_CUDA>*>(prtcls.get()),
-              params.cloudph_opts,
-              make_arrinfo(this->mem->advectee(ix::th)),
-              make_arrinfo(this->mem->advectee(ix::rv)),
-              std::map<enum libcloudphxx::lgrngn::chem_species_t, libcloudphxx::lgrngn::arrinfo_t<real_t> >()
-            );
-          assert(ftr.valid());
-        } else 
-#endif
-        {
-          prtcls->step_sync(
-            params.cloudph_opts,
-            make_arrinfo(this->mem->advectee(ix::th)),
-            make_arrinfo(this->mem->advectee(ix::rv)),
-            libcloudphxx::lgrngn::arrinfo_t<real_t>(),
-            make_arrinfo(Cx),
-            this->n_dims == 2 ? libcloudphxx::lgrngn::arrinfo_t<real_t>() : make_arrinfo(Cy),
-            make_arrinfo(Cz)
-          );
-          // microphysics could have led to rv < 0 ?
-          negtozero(this->mem->advectee(ix::rv), "rv after step sync");
-
-          nancheck(this->mem->advectee(ix::th), "th after step sync");
-          nancheck(this->mem->advectee(ix::rv), "rv after step sync");
-          negcheck(this->mem->advectee(ix::th), "th after step sync");
-          negcheck(this->mem->advectee(ix::rv), "rv after step sync");
-        }
-
-        parent_t::tend = parent_t::clock::now();
-        parent_t::tsync += std::chrono::duration_cast<std::chrono::milliseconds>( parent_t::tend - parent_t::tbeg );
-      }
-      // start recording time of the non-delayed advection step
-      parent_t::tbeg = parent_t::clock::now();
     }
     this->mem->barrier();
   }
@@ -570,14 +476,123 @@ class slvr_lgrngn : public slvr_common<ct_params_t>
 
   void hook_mixed_rhs_ante_step()
   {
-    update_rhs(this->rhs, this->dt, this->n);
-    apply_rhs(this->dt);
+    this->update_rhs(this->rhs, this->dt, 0);
+
+    // pass Eulerian fields to microphysics 
+    if (this->rank == 0) 
+    {
+      // temporarily Cx & Cz are multiplied by this->rhod ...
+      auto 
+        Cx = this->mem->GC[0](this->Cx_domain).reindex(this->zero).copy(),
+        Cy = this->mem->GC[1](this->Cy_domain).reindex(this->zero).copy(),
+        Cz = this->mem->GC[ix::w](this->Cz_domain).reindex(this->zero).copy(); 
+      nancheck(Cx, "Cx after copying from mpdata");
+      nancheck(Cy, "Cy after copying from mpdata");
+      nancheck(Cz, "Cz after copying from mpdata");
+
+      // ... and now dividing them by this->rhod (TODO: z=0 is located at k=1/2)
+      {
+        Cx.reindex(this->zero) /= (*params.rhod)(this->vert_idx);
+        Cy.reindex(this->zero) /= (*params.rhod)(this->vert_idx);
+        Cz.reindex(this->zero) /= (*params.rhod)(this->vert_idx); // TODO: should be interpolated, since theres a shift between positions of rhod and Cz
+      }
+
+      // start synchronous stuff timer
+      parent_t::tbeg = parent_t::clock::now();
+
+      using libcloudphxx::lgrngn::particles_t;
+      using libcloudphxx::lgrngn::CUDA;
+      using libcloudphxx::lgrngn::multi_CUDA;
+
+        prtcls->sync_in(
+          make_arrinfo(this->mem->advectee(ix::th)),
+          make_arrinfo(this->mem->advectee(ix::rv)),
+          libcloudphxx::lgrngn::arrinfo_t<real_t>(),
+          make_arrinfo(Cx),
+          this->n_dims == 2 ? libcloudphxx::lgrngn::arrinfo_t<real_t>() : make_arrinfo(Cy),
+          make_arrinfo(Cz)
+        );
+    }
+    this->mem->barrier();
+
+    // apply rhs (except condensation)
+    this->apply_rhs(this->dt);
+    // rv might be negative due to large negative RHS from SD fluctuations + large-scale subsidence?
+    // turn all negative rv into rv = 0... CHEATING
+    negtozero(this->mem->advectee(ix::rv), "rv before step sync");
+    nancheck(this->mem->advectee(ix::th), "th before step sync");
+    nancheck(this->mem->advectee(ix::rv), "rv before step sync");
+    negcheck(this->mem->advectee(ix::th), "th before step sync");
+    negcheck(this->mem->advectee(ix::rv), "rv before step sync");
+
+    // start sync/async run of step_cond
+    // step_cond takes th and rv only for sync_out purposes - the values of th and rv before condensation come from sync_in, i.e. before apply_rhs
+    if (this->rank == 0) 
+    {
+      using libcloudphxx::lgrngn::particles_t;
+      using libcloudphxx::lgrngn::CUDA;
+      using libcloudphxx::lgrngn::multi_CUDA;
+
+#if defined(STD_FUTURE_WORKS)
+      if (params.async)
+      {
+        assert(!ftr.valid());
+        if(params.backend == CUDA)
+          ftr = std::async(
+            std::launch::async, 
+            &particles_t<real_t, CUDA>::step_cond, 
+            dynamic_cast<particles_t<real_t, CUDA>*>(prtcls.get()),
+            params.cloudph_opts,
+            make_arrinfo(this->mem->advectee(ix::th)),
+            make_arrinfo(this->mem->advectee(ix::rv)),
+            std::map<enum libcloudphxx::lgrngn::chem_species_t, libcloudphxx::lgrngn::arrinfo_t<real_t> >()
+          );
+        else if(params.backend == multi_CUDA)
+          ftr = std::async(
+            std::launch::async, 
+            &particles_t<real_t, multi_CUDA>::step_cond, 
+            dynamic_cast<particles_t<real_t, multi_CUDA>*>(prtcls.get()),
+            params.cloudph_opts,
+            make_arrinfo(this->mem->advectee(ix::th)),
+            make_arrinfo(this->mem->advectee(ix::rv)),
+            std::map<enum libcloudphxx::lgrngn::chem_species_t, libcloudphxx::lgrngn::arrinfo_t<real_t> >()
+          );
+        assert(ftr.valid());
+      } else 
+#endif
+      {
+        prtcls->step_cond(
+          params.cloudph_opts,
+          make_arrinfo(this->mem->advectee(ix::th)),
+          make_arrinfo(this->mem->advectee(ix::rv))
+        );
+        // microphysics could have led to rv < 0 ?
+        negtozero(this->mem->advectee(ix::rv), "rv after step sync");
+
+        nancheck(this->mem->advectee(ix::th), "th after step sync");
+        nancheck(this->mem->advectee(ix::rv), "rv after step sync");
+        negcheck(this->mem->advectee(ix::th), "th after step sync");
+        negcheck(this->mem->advectee(ix::rv), "rv after step sync");
+      }
+
+      parent_t::tend = parent_t::clock::now();
+      parent_t::tsync += std::chrono::duration_cast<std::chrono::milliseconds>( parent_t::tend - parent_t::tbeg );
+      // start recording time of the non-delayed advection step
+      parent_t::tbeg = parent_t::clock::now();
+    }
+    this->mem->barrier();
   }
 
   void hook_mixed_rhs_post_step()
   {
-    update_rhs(this->rhs, this->dt, this->n+1);
-    apply_rhs(this->dt);
+    this->update_rhs(this->rhs, this->dt, 1);
+    this->apply_rhs(this->dt);
+    // no negtozero after apply, because only w changed here
+    // TODO: add these nanchecks/negchecks to apply_rhs, since they are repeated twice now
+    nancheck(this->mem->advectee(ix::th), "th after step sync");
+    nancheck(this->mem->advectee(ix::rv), "rv after step sync");
+    negcheck(this->mem->advectee(ix::th), "th after step sync");
+    negcheck(this->mem->advectee(ix::rv), "rv after step sync");
   }
   
   void record_all()
