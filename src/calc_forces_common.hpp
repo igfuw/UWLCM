e5026928296b99ac7315efc6851288bd0d24141a
pdziekan <pdziekanp@gmail.com>
2018-09-17 12:27:08 +0200

    move libcloudphxx functors to a separate file in detail/

--- src/calc_forces_common.hpp
+++ src/calc_forces_common.hpp
@@ -2,27 +2,6 @@
 #pragma once
 #include "forcings.hpp"
 
-// helper functors
-struct calc_c_p
-{
-  setup::real_t operator()(setup::real_t rv) const
-  {return libcloudphxx::common::moist_air::c_p<setup::real_t>(rv) * si::kilograms * si::kelvins / si::joules;}
-  BZ_DECLARE_FUNCTOR(calc_c_p)
-};
-
-struct calc_T
-{
-  setup::real_t operator()(setup::real_t th, setup::real_t rhod) const
-  {return libcloudphxx::common::theta_dry::T<setup::real_t>(th * si::kelvins, rhod * si::kilograms / si::metres  / si::metres / si::metres) / si::kelvins;}
-  BZ_DECLARE_FUNCTOR2(calc_T)
-};
-
-struct calc_exner
-{
-  setup::real_t operator()(setup::real_t p) const
-  {return libcloudphxx::common::theta_std::exner<setup::real_t>(p * si::pascals);}
-  BZ_DECLARE_FUNCTOR(calc_exner)
-};
 
 // common forcing functions
 // TODO: make functions return blitz arrays to avoid unnecessary copies
