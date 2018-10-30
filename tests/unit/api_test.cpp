15e152c87480b673011de784b71b7a351fa61955
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2018-10-30 15:29:03 +0100

    api tests with lower prs_tol

--- tests/unit/api_test.cpp
+++ tests/unit/api_test.cpp
@@ -15,36 +15,33 @@ int main(int ac, char** av)
 
   string outdir;
   string opts_common = 
-    "--outfreq=1000 --nt=2 --spinup=1 --dt=1 --serial=true"; // low dt to avoid pressure solver getting stuck
+    "--outfreq=1000 --nt=2 --spinup=1 --dt=1 --serial=true --prs_tol=1e-3"; 
   vector<string> opts_dim({
     "--nx=4 --nz=4",
     "--nx=4 --ny=4 --nz=4"
   });
   string opts_dim_lgrngn_3d =  "--nx=40 --ny=40 --nz=40"; // = 4 caused multiplicity overflows in the Lagrangian 3D
   vector<string> opts_micro({
-    "--micro=blk_1m --outdir=out_blk_1m"  /*,
-    "--async=false --micro=lgrngn --outdir=out_lgrngn --backend=serial --sd_conc=8 --z_rlx_sclr=100"*/
+    "--micro=blk_1m --outdir=out_blk_1m"  ,
+    "--async=false --micro=lgrngn --outdir=out_lgrngn --backend=serial --sd_conc=8 --z_rlx_sclr=100"
   });
   vector<string> opts_case({
-//    "--case=moist_thermal",
-//    "--case=dry_thermal --cond=0 --coal=0",
+    "--case=moist_thermal",
+    "--case=dry_thermal --cond=0 --coal=0",
     "--case=dycoms"
   });
   vector<string> opts_piggy({
-    "--piggy=0"//,
-//    "--piggy=0 --save_vel=1",
-//    "--piggy=1 --vel_in=out_blk_1m/velocity_out.dat"  // take vel file from blk, cause it's ran first
+    "--piggy=0",
+    "--piggy=0 --save_vel=1",
+    "--piggy=1 --vel_in=out_blk_1m/velocity_out.dat"  // take vel file from blk, cause it's ran first
   });
   string opts_pig_from_lgrngn = "--piggy=1 --vel_in=out_lgrngn/velocity_out.dat"; // 3d dycoms lgrgn has more cells, se we cant piggyback on blk_1m
-  string opts_dim_blk_1m_dycoms_2d =  "--nx=40 --nz=40"; // = 4 made pressure solver stuck due to -rl_e component in buoyancy
-  string opts_dim_blk_1m_dycoms_3d =  "--nx=40 --ny=40 --nz=40"; // = 4 made pressure solver stuck due to -rl_e component in buoyancy
 
   for (auto &opts_d : opts_dim)
     for (auto &opts_m : opts_micro)
       for (auto &opts_c : opts_case)
         for (auto &opts_p : opts_piggy) // piggy has to be last to prevent overwriting of vel_out
         {
-/*
           if((opts_c == opts_case[1]) && opts_d == opts_dim[1])
           {
             std::cout << "skipping 3d dry thermal tests" << std::endl;
@@ -57,16 +54,6 @@ int main(int ac, char** av)
             opts_d = opts_dim_lgrngn_3d;
             if(opts_p == opts_piggy[2])
               opts_p = opts_pig_from_lgrngn;
-          }
-*/
-          // more cells in blk_1m dycoms to avoid perssure solver freezes
-//          if(opts_m == opts_micro[0] && opts_c == opts_case[2])
-          if(opts_m == opts_micro[0] && opts_c == opts_case[0])
-          {
-            if(opts_d == opts_dim[0])
-              opts_d = opts_dim_blk_1m_dycoms_2d;
-            if(opts_d == opts_dim[1])
-              opts_d = opts_dim_blk_1m_dycoms_3d;
           }
           ostringstream cmd;
           cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m << " " << opts_d << " " << opts_c << " " << opts_p;
