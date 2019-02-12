d617f66d4a3ceabeb2895e08a0384f22eebc2887
pdziekan <pdziekanp@gmail.com>
2019-02-12 15:03:17 +0100

    travis scripts: fix directories

--- .travis_scripts/moist_thermal.sh
+++ .travis_scripts/moist_thermal.sh
@@ -2,13 +2,12 @@
 set -ex
 
 # UWLCM in RelWithDebInfo mode without 3D and with 'abs' libmpdata++ option
-cd UWLCM
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUWLCM_DISABLE="2D_LGRNGN;2D_BLK_1M" -DMPDATA_OPTS="ABS"
 VERBOSE=1 make
 cd tests/moist_thermal
 OMP_NUM_THREADS=6 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
-cd ../..
+cd ../../..
 
 set +ex # see https://github.com/travis-ci/travis-ci/issues/6522
--- .travis_scripts/unit.sh
+++ .travis_scripts/unit.sh
@@ -2,13 +2,12 @@
 set -ex
 
 # UWLCM in RelWithDebInfo mode
-cd UWLCM
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo
 VERBOSE=1 make
 cd tests/unit
 OMP_NUM_THREADS=1 make test || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
-cd ../..
+cd ../../..
 
 set +ex # see https://github.com/travis-ci/travis-ci/issues/6522
