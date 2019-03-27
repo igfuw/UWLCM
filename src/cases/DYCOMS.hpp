edbaa034deb2f8f8b6ab9415bdb7814170fe80bd
pdziekan <pdziekanp@gmail.com>
2019-03-27 14:48:24 +0100

    dycoms: set geostrophic winds only in 3d

--- src/cases/DYCOMS.hpp
+++ src/cases/DYCOMS.hpp
@@ -261,12 +261,6 @@ namespace setup
 //        for(int k=1; k<nz; ++k)
   //        th_e(k) = theta_dry::std2dry<real_t>(th_e(k) * si::kelvins, quantity<si::dimensionless, real_t>(rv_e(k))) / si::kelvins;
 
-        // Coriolis parameter
-
-
-        // geostrophic wind equal to the initial velocity profile
-        profs.geostr[0] = u()(k * dz); 
-        profs.geostr[1] = v()(k * dz); 
   
         // subsidence rate
         profs.w_LS = w_LS_fctr()(k * dz);
@@ -399,6 +393,17 @@ namespace setup
   
         solver.advectee(ix::v)= v()(k * dz);
         solver.vab_relaxed_state(1) = solver.advectee(ix::v);
+      }
+
+      void env_prof(profiles_t &profs, int nz, const user_params_t &user_params)
+      {
+        parent_t::env_prof(profs, nz, user_params);
+        // geostrophic wind equal to the initial velocity profile
+        blitz::thirdIndex k;
+        typename parent_t::u u;
+        real_t dz = (Z / si::metres) / (nz-1);
+        profs.geostr[0] = u(k * dz); 
+        profs.geostr[1] = v()(k * dz); 
       }
     };
   };
