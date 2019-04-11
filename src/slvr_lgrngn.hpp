6a4553f16880394d6a53eaedf485ee0201ef641d
Piotr Dziekan <pdziekanp@gmail.com>
2019-04-11 11:40:23 +0200

    turb_adve/cond+switch saved to const.h5

--- src/slvr_lgrngn.hpp
+++ src/slvr_lgrngn.hpp
@@ -402,10 +402,14 @@ class slvr_lgrngn : public std::conditional_t<ct_params_t::sgs_scheme == libmpda
       this->record_aux_const("cond", params.cloudph_opts.cond);  
       this->record_aux_const("coal", params.flag_coal);  // cloudph_opts.coal could be 0 here due to spinup
       this->record_aux_const("rcyc", params.cloudph_opts.rcyc);  
+      this->record_aux_const("turb_adve", params.cloudph_opts.turb_adve);  
+      this->record_aux_const("turb_cond", params.cloudph_opts.turb_cond);  
       this->record_aux_const("chem_switch", params.cloudph_opts_init.chem_switch);  
       this->record_aux_const("coal_switch", params.cloudph_opts_init.coal_switch);  
       this->record_aux_const("sedi_switch", params.cloudph_opts_init.sedi_switch);  
       this->record_aux_const("src_switch", params.cloudph_opts_init.src_switch);  
+      this->record_aux_const("turb_adve_switch", params.cloudph_opts_init.turb_adve_switch);  
+      this->record_aux_const("turb_cond_switch", params.cloudph_opts_init.turb_cond_switch);  
       this->record_aux_const("chem_dsl", params.cloudph_opts.chem_dsl);  
       this->record_aux_const("chem_dsc", params.cloudph_opts.chem_dsc);  
       this->record_aux_const("chem_rct", params.cloudph_opts.chem_rct);  
