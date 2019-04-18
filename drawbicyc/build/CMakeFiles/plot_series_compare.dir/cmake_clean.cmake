e2b3835af02f150124f24cc99c5e86c30ee034ab
Piotr Dziekan <pdziekan@igf.fuw.edu.pl>
2019-04-18 16:46:29 +0200

    plot series: fix surf and acc precip plots for both micro

---
+++ drawbicyc/build/CMakeFiles/plot_series_compare.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/plot_series_compare.dir/plot_series_compare.o"
+  "plot_series_compare.pdb"
+  "plot_series_compare"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/plot_series_compare.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- drawbicyc/plot_series.hpp
+++ drawbicyc/plot_series.hpp
@@ -479,13 +479,7 @@ void plot_series(Plotter_t plotter, Plots plots)
         // surface precipitation [mm/day]
         try
         {
-       //   res_prof(at) = (prec_vol - prec_vol_prev) / plotter.DomainSurf / (double(n["outfreq"]) * n["dt"] / 3600. / 24.) * 1e3; // SDM
-          res_prof(at) = (prec_vol - prec_vol_prev) / double(n["outfreq"]) // flux in [kg / m^3 / s] averaged over time since last output and over cells on the bottom
-                         / (n["x"] * n["y"])
-                         * 3600. * 24. // per day
-                         * n["dz"]     // per m^2
-                         / 1e3         // to m^3 of water
-                         * 1e3;        // to mm
+          res_prof(at) = plotter.calc_surf_precip(prec_vol - prec_vol_prev);
         }
         catch(...) {;}
       }
@@ -494,8 +488,7 @@ void plot_series(Plotter_t plotter, Plots plots)
         // accumulated surface precipitation [mm]
         try
         {
-         //   res_prof(at) = prec_vol / plotter.DomainSurf * 1e3; 
-            res_prof(at) = prec_vol * n["dt"] * n["dz"] * 1e3; 
+          res_prof(at) = plotter.calc_acc_surf_precip(prec_vol);
         }
         catch(...) {;}
       }
