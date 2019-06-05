d028349f386e457c1cd3fe84255e6d5f8c1d2c29
pdziekan <pdziekanp@gmail.com>
2019-06-05 15:11:04 +0200

    track travis unit_sgs

---
+++ .travis_scripts/unit_sgs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -ex
+
+# UWLCM in RelWithDebInfo mode
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUWLCM_DISABLE="ILES"
+VERBOSE=1 make
+cd tests/unit
+OMP_NUM_THREADS=1 ctest -R api_test_sgs || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
+cd ../../..
+
+set +ex # see https://github.com/travis-ci/travis-ci/issues/6522
