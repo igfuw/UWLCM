2d8286f5cc5b3b5efe398caa154cb9b603e75fea
pdziekan <pdziekanp@gmail.com>
2019-06-12 17:27:38 +0200

    wip: test output of api test

--- tests/unit/api_test.cpp
+++ tests/unit/api_test.cpp
@@ -14,29 +14,32 @@ int main(int ac, char** av)
   if (ac != 2 && ac != 3) error_macro("expecting one or two arguments: 1. CMAKE_BINARY_DIR 2. additional command line options (optional)");
   string opts_additional = ac == 3 ? av[2] : "";
 
-  string outdir;
   string opts_common = 
-    "--outfreq=1000 --nt=2 --spinup=1 --dt=1 --serial=true --prs_tol=1e-3"; 
+    "--outfreq=1000 --nt=2 --spinup=1 --dt=1 --serial=true --prs_tol=1e-3 --rng_seed=44"; 
   vector<string> opts_dim({
     "--nx=4 --nz=4",
     "--nx=4 --ny=4 --nz=4"
   });
   string opts_dim_lgrngn_3d =  "--nx=40 --ny=40 --nz=40"; // = 4 caused multiplicity overflows in the Lagrangian 3D
   vector<string> opts_micro({
-    "--micro=blk_1m --outdir=out"  ,
-    "--async=false --micro=lgrngn --outdir=out --backend=serial --sd_conc=8 --z_rlx_sclr=100"
+    "--micro=blk_1m"  ,
+    "--async=false --micro=lgrngn --backend=serial --sd_conc=8 --z_rlx_sclr=100"
   });
   vector<string> opts_case({
     "--case=moist_thermal",
     "--case=dry_thermal --cond=0 --coal=0",
-    "--case=dycoms_rf02"
+//    "--case=dycoms_rf01",
+    "--case=dycoms_rf02",
+//    "--case=lasher_trapp"
   });
   vector<string> opts_piggy({
     "--piggy=0",
     "--piggy=0 --save_vel=1",
-    "--piggy=1 --vel_in=out/velocity_out.dat"  // take vel file from blk, cause it's ran first
+    "--piggy=1 --vel_in=velocity_out.dat"  // take vel file from blk, cause it's ran first
   });
-  string opts_pig_from_lgrngn = "--piggy=1 --vel_in=out/velocity_out.dat";
+  string opts_pig_from_lgrngn = "--piggy=1 --vel_in=velocity_out.dat";
+
+  system("mkdir output");
 
   for (auto &opts_d : opts_dim)
     for (auto &opts_m : opts_micro)
@@ -56,11 +59,21 @@ int main(int ac, char** av)
             if(opts_p == opts_piggy[2])
               opts_p = opts_pig_from_lgrngn;
           }
-          ostringstream cmd;
-          cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m << " " << opts_d << " " << opts_c << " " << opts_p << " " << opts_additional;
+          ostringstream cmd, opts;
+          opts << opts_common << " " << opts_m << " " << opts_d << " " << opts_c << " " << opts_p << " " << opts_additional;
+          cmd << av[1] << "/src/bicycles " << opts.str() << " --outdir=\"output/" << opts.str() << "\"";
           notice_macro("about to call: " << cmd.str())
   
           if (EXIT_SUCCESS != system(cmd.str().c_str()))
             error_macro("model run failed: " << cmd.str())
+
+          // copy the stored velocity for the next run
+          if(opts_p == opts_piggy[1])
+          {
+            ostringstream cpcmd;
+            cpcmd << "cp \"output/" << opts.str() << "/velocity_out.dat\" .";
+            notice_macro("about to call: " << cpcmd.str())
+            system(cpcmd.str().c_str());
+          }
         }
 }
