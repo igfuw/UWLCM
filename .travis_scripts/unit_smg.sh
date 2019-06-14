e37f9e5cad3e440b0c14f1eb86abab7cf335b0f3
pdziekan <pdziekanp@gmail.com>
2019-06-14 11:03:03 +0200

    unit test fixes for travis

--- .travis_scripts/unit_smg.sh
+++ .travis_scripts/unit_smg.sh
@@ -7,7 +7,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUWLCM_DISABLE="ILES"
 VERBOSE=1 make
 cd tests/unit
-OMP_NUM_THREADS=1 ctest -R api_test_sgs || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
+OMP_NUM_THREADS=1 ctest -R api_test_smg || cat Testing/Temporary/LastTest.log / # "/" intentional! (just to make cat exit with an error code)
 cd ../../..
 
 set +ex # see https://github.com/travis-ci/travis-ci/issues/6522
