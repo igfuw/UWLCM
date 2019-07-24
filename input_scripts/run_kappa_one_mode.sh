af42e699d26f56f04a3d87251f4f2638a93e9801
claresinger <csinger@caltech.edu>
2019-07-24 16:36:03 -0700

    change opts_lgrngn to only take one mode for now

--- input_scripts/run_kappa_one_mode.sh
+++ input_scripts/run_kappa_one_mode.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # Submit this script with: sbatch <this-filename>
-##SBATCH --account=seinfeldgroup 		# accounting resource
-#SBATCH --job-name=test-kappa			# job name
+#SBATCH --account=seinfeldgroup 		# accounting resource
+#SBATCH --job-name=var-mu-kappa			# job name
 
-#SBATCH --time=00:30:00				# walltime
+#SBATCH --time=03:00:00				# walltime
 #SBATCH --nodes=1				# 1 GPU node
 #SBATCH --ntasks=1				# 1 CPU core to drive GPU
 #SBATCH --gres=gpu:1				# request 1 GPU
 
-##SBATCH --exclusive				# for exclusive use of node
-##SBATCH --qos=debug				# debug mode
-
-##SBATCH --mail-user=csinger@caltech.edu   	# email address
-##SBATCH --mail-type=ALL			# notify all
+#SBATCH --mail-user=csinger@caltech.edu   	# email address
+#SBATCH --mail-type=ALL				# notify all
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 module load singularity/3.2.1
@@ -25,42 +22,37 @@ CONTAINER=$HOME/sng_output.sif
 MODEL=$HOME/UWLCM/build/src/bicycles
 
 # aerosol distribution params
-MU1=0.1e-6
+MU1=0.01e-6
 SIG1=1.2
-N1=10e6
-KAP1=0.61
-
-MU2=0.8e-6
-SIG2=1.5
-N2=10e6
-KAP2=1.28
+N1=100e6
+KAP1=0.1
 
 # run time params
-SPIN=360
-NT=2160
+SPIN=3600
+NT=21600
 OUTFREQ=300
 
 # case params
-NX=33
-NZ=76
+NX=129
+NZ=301
 
 # output directory
-OUTPUT_DIR=$HOME/output/test1
+OUTPUT_DIR=$HOME/output/mu_${MU1}_kappa_${KAP1}/exp1
 
-# copy this input script to the output directory
-mkdir $OUTPUT_DIR
-cp ${0} $OUTPUT_DIR/input.sh
-
-#set param strings
+# set param strings
 CASE_PARAMS="--case=dycoms_rf02 --nx=$NX --ny=0 --nz=$NZ"
 RUN_PARAMS="--dt=1 --spinup=$SPIN --nt=$NT --outfreq=$OUTFREQ --backend=CUDA"
 COMMON_PARAMS="--sstp_cond=10 --sstp_coal=10 --rng_seed=0"
-MICRO_PARAMS="--micro=lgrngn --mean_rd1=$MU1 --sdev_rd1=$SIG1 --n1_stp=$N1 --kappa1=$KAP1 --mean_rd2=$MU2 --sdev_rd2=$SIG2 --n2_stp=$N2 --kappa2=$KAP2 --sd_conc=40"
+MICRO_PARAMS="--micro=lgrngn --mean_rd1=$MU1 --sdev_rd1=$SIG1 --n1_stp=$N1 --kappa1=$KAP1 --sd_conc=40"
 
 wet_bins_str=$(python make_bins.py "wet")
 dry_bins_str=$(python make_bins.py "dry")
 
 BINS="--out_wet=$wet_bins_str --out_dry=$dry_bins_str"
+
+# copy this input script to the output directory
+mkdir $OUTPUT_DIR
+cp ${0} $OUTPUT_DIR/input.sh
 
 # make .txt file in output directory with wet_bins_str and dry_bins_str
 BINS_FILE="bin_strs.txt"
@@ -71,8 +63,9 @@ echo $dry_bins_str >> $OUTPUT_DIR/$BINS_FILE
 chmod -w $OUTPUT_DIR/$BINS_FILE
 
 # run program (-d == in debug mode)
-echo starting job $SLURM_JOB_ID
-srun singularity -d exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS $BINS
+echo about to call bicycles...
+echo job id = ${SLURM_JOB_ID}
+srun singularity exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS $BINS
 echo done!
 
 # copy slurm file to output directory
