7351a93073691c24a24c3851541a86056e1ba5ca
pdziekan <pdziekanp@gmail.com>
2019-08-09 14:44:48 +0200

    travis: mpi build boost atomic and regex

--- .travis_scripts/install_mpi_dependencies.sh
+++ .travis_scripts/install_mpi_dependencies.sh
@@ -53,12 +53,12 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then sudo $apt_get_install python-numpy; fi
       echo "using mpi : $CC ;" >> $HOME/user-config.jam
       cat $HOME/user-config.jam
       if [[ $COMPILER == 'g++' ]]; then
-        ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=serialization,mpi,thread,date_time,system,iostreams,timer,filesystem,program_options,python
+        ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=serialization,mpi,thread,date_time,system,iostreams,timer,filesystem,program_options,python,atomic,regex
         ./b2 -d0 install
       fi
       if [[ $COMPILER == 'clang++' ]]; then 
         #clang installation taken from https://gist.github.com/jimporter/10442880
-        ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=serialization,mpi,thread,date_time,system,iostreams,timer,filesystem,program_options,python --with-toolset=clang
+        ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=serialization,mpi,thread,date_time,system,iostreams,timer,filesystem,program_options,python,atomic,regex --with-toolset=clang
         ./b2 clean
         ./b2 toolset=clang cxxflags="-std=c++14 -stdlib=libc++" linkflags="-stdlib=libc++" --prefix=${DEPS_DIR}/boost/ -j 4 stage release
         ./b2 install toolset=clang cxxflags="-std=c++14 -stdlib=libc++" linkflags="-stdlib=libc++" --prefix=${DEPS_DIR}/boost/
