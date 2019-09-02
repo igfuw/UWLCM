46841e5c180c41bd02de95c362009cd430e88136
pdziekan <pdziekanp@gmail.com>
2019-09-02 14:37:48 +0200

    Revert "Revert "travis mt mpi test: omp6""
    This reverts commit d837264bbe56f53bea73eaf245f9ff01d0318047.

--- .travis_scripts/moist_thermal.sh
+++ .travis_scripts/moist_thermal.sh
@@ -8,7 +8,7 @@ cd build
 cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DUWLCM_DISABLE="3D_LGRNGN;3D_BLK_1M;PIGGYBACKER;SGS" -DMPDATA_OPTS="ABS"
 VERBOSE=1 make -j2
 cd tests/moist_thermal
-if [[ $MPI != 'none'    ]]; then OMP_NUM_THREADS=2 travis_wait 30 make test || cat Testing/Temporary/LastTest.log /; fi # "/" intentional! (just to make cat exit with an error code)
+if [[ $MPI != 'none'    ]]; then OMP_NUM_THREADS=6 travis_wait 30 make test || cat Testing/Temporary/LastTest.log /; fi # "/" intentional! (just to make cat exit with an error code)
 if [[ $MPI == 'none'    ]]; then OMP_NUM_THREADS=6 make test || cat Testing/Temporary/LastTest.log /; fi # "/" intentional! (just to make cat exit with an error code)
 cd ../../..
 
