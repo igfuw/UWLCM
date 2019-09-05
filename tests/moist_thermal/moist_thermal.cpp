161c116a6f27f156350ff9bf34a2ea42d7f1b8e6
pdziekan <pdziekanp@gmail.com>
2019-09-05 12:02:54 -0600

    mt test: nz 121

--- tests/moist_thermal/moist_thermal.cpp
+++ tests/moist_thermal/moist_thermal.cpp
@@ -45,7 +45,7 @@ int main(int ac, char** av)
 
   string outdir;
   string opts_common = 
-    "--outfreq=60 --nt=600 --spinup=0 --dt=1 --nx=181 --nz=181 --case=moist_thermal";
+    "--outfreq=60 --nt=600 --spinup=0 --dt=1 --nx=181 --nz=121 --case=moist_thermal";
   unordered_map<string, string> opts_micro({
     {"blk_1m", "--micro=blk_1m --outdir=tmp_out_blk_1m --cond=true --cevp=true --revp=false --conv=false --accr=false --sedi=false"},
     {"lgrngn", "--micro=lgrngn --outdir=tmp_out_lgrngn --cond=true --adve=true --sedi=false --coal=false --backend=OpenMP --sd_conc=16 --rng_seed=44"}
