b393bee28e47ac322c4cee7cd79904a74b378ccf
pdziekan <pdziekanp@gmail.com>
2019-10-14 12:42:51 +0200

    surf flux uv: uv at lvl 1

--- src/forcings/surface_fluxes.hpp
+++ src/forcings/surface_fluxes.hpp
@@ -54,7 +54,7 @@ void slvr_common<ct_params_t>::surf_u_impl(iles_tag)
 {
   params.update_surf_flux_uv(
     surf_flux_u(this->hrzntl_slice(0)).reindex(this->origin),
-    this->state(ix::vip_i)(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::vip_i)(this->hrzntl_slice(1)).reindex(this->origin),
     U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
     this->timestep, this->dt, this->di, this->dj
   );
@@ -82,7 +82,7 @@ void slvr_common<ct_params_t>::surf_v_impl(iles_tag)
 {
   params.update_surf_flux_uv(
     surf_flux_v(this->hrzntl_slice(0)).reindex(this->origin),
-    this->state(ix::vip_j)(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::vip_j)(this->hrzntl_slice(1)).reindex(this->origin),
     U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
     this->timestep, this->dt, this->di, this->dj
   );
