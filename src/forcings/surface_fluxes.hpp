04b76470f7d260ec1cc5d2ba5df6bf7ae87e12da
pdziekan <pdziekanp@gmail.com>
2019-10-15 15:30:33 +0200

    proper update surf flux calls for th and rv

--- src/forcings/surface_fluxes.hpp
+++ src/forcings/surface_fluxes.hpp
@@ -5,7 +5,13 @@
 template <class ct_params_t>
 void slvr_common<ct_params_t>::surf_sens_impl(iles_tag)
 {
-  params.update_surf_flux_sens(surf_flux_sens(this->hrzntl_slice(0)).reindex(this->origin), this->timestep, this->dt, this->di, this->dj);
+  params.update_surf_flux_sens(
+    surf_flux_sens(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::th)(this->hrzntl_slice(1)).reindex(this->origin),
+    U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
+    this->timestep, this->dt, this->di, this->dj
+  );
+
   for (auto k = this->vert_rng.first(); k <= this->vert_rng.last(); ++k)
   {
     F(this->hrzntl_slice(k)) = surf_flux_sens(this->hrzntl_slice(0)) * (*params.hgt_fctr)(k);
@@ -15,7 +21,13 @@ void slvr_common<ct_params_t>::surf_sens_impl(iles_tag)
 template <class ct_params_t>
 void slvr_common<ct_params_t>::surf_sens_impl(smg_tag)
 {
-  params.update_surf_flux_sens(surf_flux_sens(this->hrzntl_slice(0)).reindex(this->origin), this->timestep, this->dt, this->di, this->dj);
+  params.update_surf_flux_sens(
+    surf_flux_sens(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::th)(this->hrzntl_slice(1)).reindex(this->origin),
+    U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
+    this->timestep, this->dt, this->di, this->dj
+  );
+
   F(this->ijk) = 0;
 }
 
@@ -28,7 +40,13 @@ void slvr_common<ct_params_t>::surf_sens()
 template <class ct_params_t>
 void slvr_common<ct_params_t>::surf_latent_impl(iles_tag)
 {
-  params.update_surf_flux_lat(surf_flux_lat(this->hrzntl_slice(0)).reindex(this->origin), this->timestep, this->dt, this->di, this->dj);
+  params.update_surf_flux_lat(
+    surf_flux_lat(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::rv)(this->hrzntl_slice(1)).reindex(this->origin) + this->r_l(this->hrzntl_slice(1)).reindex(this->origin), // when was r_l diagnosed? this or previous step? probably makes no difference (we are on the ground)
+    U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
+    this->timestep, this->dt, this->di, this->dj
+  );
+
   for (auto k = this->vert_rng.first(); k <= this->vert_rng.last(); ++k)
   {
     F(this->hrzntl_slice(k)) = surf_flux_lat(this->hrzntl_slice(0)) * (*params.hgt_fctr)(k);
@@ -38,7 +56,13 @@ void slvr_common<ct_params_t>::surf_latent_impl(iles_tag)
 template <class ct_params_t>
 void slvr_common<ct_params_t>::surf_latent_impl(smg_tag)
 {
-  params.update_surf_flux_lat(surf_flux_lat(this->hrzntl_slice(0)).reindex(this->origin), this->timestep, this->dt, this->di, this->dj);
+  params.update_surf_flux_lat(
+    surf_flux_lat(this->hrzntl_slice(0)).reindex(this->origin),
+    this->state(ix::rv)(this->hrzntl_slice(1)).reindex(this->origin) + this->r_l(this->hrzntl_slice(1)).reindex(this->origin), // when was r_l diagnosed? this or previous step? probably makes no difference (we are on the ground)
+    U_ground(this->hrzntl_slice(0)).reindex(this->origin), params.dz,
+    this->timestep, this->dt, this->di, this->dj
+  );
+
   F(this->ijk) = 0;
 }
 
