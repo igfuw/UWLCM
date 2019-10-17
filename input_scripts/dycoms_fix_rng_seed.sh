7a93bfdf7bb0d78dabcd4f0ade587c6d9ef34dc9
claresinger <csinger@caltech.edu>
2019-10-16 20:11:02 -0700

    update input scripts

--- input_scripts/dycoms_fix_rng_seed.sh
+++ input_scripts/dycoms_fix_rng_seed.sh
@@ -8,6 +8,7 @@
 #SBATCH --nodes=1				# 1 GPU node
 #SBATCH --ntasks=1				# 1 CPU core to drive GPU
 #SBATCH --gres=gpu:1				# request 1 GPU
+#SBATCH --qos=debug				# debug mode
 
 #SBATCH --mail-user=csinger@caltech.edu   	# email address
 #SBATCH --mail-type=ALL				# notify all
@@ -42,37 +43,37 @@ NX=129
 NZ=301
 
 # output directory
-OUTPUT_DIR=$HOME/output/dycoms_test_fix_seed/exp5-newrico
+OUTPUT_DIR=$HOME/output/dycoms_test_fix_seed/exp5-explicit_aerosols_rico
 
 # set param strings
 CASE_PARAMS="--case=dycoms_rf02 --nx=$NX --ny=0 --nz=$NZ"
 RUN_PARAMS="--dt=1 --spinup=$SPIN --nt=$NT --outfreq=$OUTFREQ --backend=CUDA"
 COMMON_PARAMS="--sstp_cond=10 --sstp_coal=10 --rng_seed=42"
-#MICRO_PARAMS="--micro=lgrngn --mean_rd1=$MU1 --sdev_rd1=$SIG1 --n1_stp=$N1 --kappa1=$KAP1 --mean_rd2=$MU2 --sdev_rd2=$SIG2 --n2_stp=$N2 --kappa2=$KAP2 --sd_conc=40"
-MICRO_PARAMS="--micro=lgrngn --sd_conc=40"
+MICRO_PARAMS="--micro=lgrngn --mean_rd1=$MU1 --sdev_rd1=$SIG1 --n1_stp=$N1 --kappa1=$KAP1 --mean_rd2=$MU2 --sdev_rd2=$SIG2 --n2_stp=$N2 --kappa2=$KAP2 --sd_conc=40"
+#MICRO_PARAMS="--micro=lgrngn --sd_conc=40"
 
-#wet_bins_str=$(python make_bins.py "wet")
-#dry_bins_str=$(python make_bins.py "dry")
+wet_bins_str=$(python make_bins.py "wet")
+dry_bins_str=$(python make_bins.py "dry")
 
-#BINS="--out_wet=$wet_bins_str --out_dry=$dry_bins_str"
+BINS="--out_wet=$wet_bins_str --out_dry=$dry_bins_str"
 
 # copy this input script to the output directory
 mkdir -p $OUTPUT_DIR
 cp ${0} $OUTPUT_DIR/input.sh
 
 # make .txt file in output directory with wet_bins_str and dry_bins_str
-#BINS_FILE="bin_strs.txt"
-#echo wet_bins_str: >> $OUTPUT_DIR/$BINS_FILE
-#echo $wet_bins_str >> $OUTPUT_DIR/$BINS_FILE
-#echo dry_bins_str: >> $OUTPUT_DIR/$BINS_FILE
-#echo $dry_bins_str >> $OUTPUT_DIR/$BINS_FILE
-#chmod -w $OUTPUT_DIR/$BINS_FILE
+BINS_FILE="bin_strs.txt"
+echo wet_bins_str: >> $OUTPUT_DIR/$BINS_FILE
+echo $wet_bins_str >> $OUTPUT_DIR/$BINS_FILE
+echo dry_bins_str: >> $OUTPUT_DIR/$BINS_FILE
+echo $dry_bins_str >> $OUTPUT_DIR/$BINS_FILE
+chmod -w $OUTPUT_DIR/$BINS_FILE
 
 # run program (-d == in debug mode)
 echo about to call bicycles...
 echo job id = ${SLURM_JOB_ID}
-#srun singularity exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS $BINS
-srun singularity exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS
+srun singularity exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS $BINS
+#srun singularity exec --nv $CONTAINER $MODEL --outdir=$OUTPUT_DIR $CASE_PARAMS $RUN_PARAMS $COMMON_PARAMS $MICRO_PARAMS
 echo done!
 
 # copy slurm file to output directory
