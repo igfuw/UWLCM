234c8feb4c8bc9bee7fe9c6f856ef07598301a2e
pdziekan <pdziekanp@gmail.com>
2019-11-04 12:38:42 +0100

    fix non-api_test case choice

--- src/run_hlpr.cpp
+++ src/run_hlpr.cpp
@@ -76,9 +76,8 @@ void run(const int (&nps)[n_dims], const user_params_t &user_params)
     case_ptr.reset(new setup::LasherTrapp::LasherTrapp2001<case_ct_params_t, n_dims>());
   else if (user_params.model_case == "rico11")
     case_ptr.reset(new setup::rico::Rico11<case_ct_params_t, n_dims>());
-
   // special versions for api test - they have much lower aerosol concentrations to avoid multiplicity overflow
-  if (user_params.model_case == "moist_thermal_api_test")
+  else if (user_params.model_case == "moist_thermal_api_test")
     case_ptr.reset(new setup::api_test<setup::moist_thermal::MoistThermalGrabowskiClark99<case_ct_params_t, n_dims>>()); 
   else if (user_params.model_case == "dry_thermal_api_test")
     case_ptr.reset(new setup::api_test<setup::dry_thermal::DryThermal<case_ct_params_t, n_dims>>()); 
