a5c93b90d9e90a44c0a697a35fb347373d0ddc23
claresinger <csinger@caltech.edu>
2019-11-05 15:12:44 -0800

    update api_test to add user_params so you can set n1 and n2

--- src/cases/detail/api_test.hpp
+++ src/cases/detail/api_test.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../../detail/user_params.hpp"
+
 namespace setup 
 {
   // special case for api tests - low aerosol concentration to avoid multiplicity overflows in tests with very low nx/ny/nz
@@ -7,10 +9,16 @@ namespace setup
   class api_test : public case_t
   {
     public:
+    
+    user_params_t user_params;
+
     api_test()
     {
-      this->n1_stp = real_t(2) / si::cubic_metres;
-      this->n2_stp = real_t(1) / si::cubic_metres;
+      //this->user_params.n1_stp = real_t(2) / si::cubic_metres;
+      //this->user_params.n2_stp = real_t(1) / si::cubic_metres;
+      
+      user_params.n1_stp = real_t(2) / si::cubic_metres;
+      user_params.n2_stp = real_t(1) / si::cubic_metres;
     }
   };
 };
