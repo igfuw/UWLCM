5cede1b4747987b4810721acf089ee868f5073e0
pdziekan <pdziekanp@gmail.com>
2019-11-13 13:00:39 +0100

    turn on buoyancy smoothing

--- src/forcings/buoyancy.hpp
+++ src/forcings/buoyancy.hpp
@@ -12,15 +12,16 @@ void slvr_common<ct_params_t>::buoyancy(typename parent_t::arr_t &th, typename p
   namespace moist_air = libcloudphxx::common::moist_air;
   const real_t eps = moist_air::R_v<real_t>() / moist_air::R_d<real_t>() - 1.;
   if(params.buoyancy_wet)
-    F(ijk).reindex(this->zero) = 
+    tmp1(ijk).reindex(this->zero) = 
       (libcloudphxx::common::earth::g<setup::real_t>() / si::metres_per_second_squared) * (
         (th(ijk).reindex(this->zero) - (*params.th_e)(this->vert_idx)) / (*params.th_ref)(this->vert_idx)
         + eps * (rv(ijk).reindex(this->zero) - (*params.rv_e)(this->vert_idx)) 
         - (r_l(ijk).reindex(this->zero) - (*params.rl_e)(this->vert_idx))
       );
   else
-    F(ijk).reindex(this->zero) = 
+    tmp1(ijk).reindex(this->zero) = 
       (libcloudphxx::common::earth::g<setup::real_t>() / si::metres_per_second_squared) * (
         (th(ijk).reindex(this->zero) - (*params.th_e)(this->vert_idx)) / (*params.th_ref)(this->vert_idx)
       );
+  this->smooth(tmp1, F);
 }
