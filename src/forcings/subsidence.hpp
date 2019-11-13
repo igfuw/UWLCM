9d6d6ee6dbc606133766f3cb5092ebeea33af550
pdziekan <pdziekanp@gmail.com>
2019-11-13 13:00:30 +0100

    comment in subsidence

--- src/forcings/subsidence.hpp
+++ src/forcings/subsidence.hpp
@@ -8,8 +8,8 @@ void slvr_common<ct_params_t>::subsidence(const int &type) // large-scale vertic
   const auto &ijk = this->ijk;
   if(params.subsidence)
   {
-    tmp1(ijk) = this->state(type)(ijk);
-    this->vert_grad_cnt(tmp1, F, params.dz);
+    tmp1(ijk) = this->state(type)(ijk); // TODO: no need to copy to tmp array?
+    this->vert_grad_cnt(tmp1, F, params.dz); 
     F(ijk).reindex(this->zero) *= - (*params.w_LS)(this->vert_idx);
 
 //    tmp1(ijk)=F(ijk); //TODO: unnecessary copy
