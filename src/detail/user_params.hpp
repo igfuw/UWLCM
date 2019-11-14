a813065bbf802786224344183f1aa2b534126b13
claresinger <csinger@caltech.edu>
2019-11-14 12:36:26 -0800

    add 2moment user_params

--- src/detail/user_params.hpp
+++ src/detail/user_params.hpp
@@ -10,6 +10,6 @@ struct user_params_t
   int nt, outfreq, spinup, rng_seed;
   setup::real_t dt;
   std::string outdir, model_case;
-  bool th_src, rv_src, rc_src, rr_src, uv_src, w_src;
+  bool th_src, rv_src, rc_src, rr_src, nc_src, nr_src, uv_src, w_src;
   setup::real_t sgs_delta;
 };
