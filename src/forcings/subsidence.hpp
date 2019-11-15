4ebbcb0c6279428f095ba3e6d1fa3cd5173e4672
pdziekan <pdziekanp@gmail.com>
2019-11-15 11:00:38 +0100

    disable subsidence smoothing

--- src/forcings/subsidence.hpp
+++ src/forcings/subsidence.hpp
@@ -8,10 +8,10 @@ void slvr_common<ct_params_t>::subsidence(const int &type) // large-scale vertic
   const auto &ijk = this->ijk;
   if(params.subsidence)
   {
-    F(ijk) = this->state(type)(ijk);
-    this->vert_grad_cnt(F, tmp1, params.dz); 
-    tmp1(ijk).reindex(this->zero) *= - (*params.w_LS)(this->vert_idx);
-    this->smooth(tmp1, F);
+    tmp1(ijk) = this->state(type)(ijk);
+    this->vert_grad_cnt(tmp1, F, params.dz); 
+    F(ijk).reindex(this->zero) *= - (*params.w_LS)(this->vert_idx);
+    //this->smooth(tmp1, F);
   }
   else
     F(ijk)=0.;
