e0bf63ed74e2b76e70f56dff8dcdd630293633b1
claresinger <csinger@caltech.edu>
2019-11-21 11:48:24 -0800

    add aerosol distribution params to user_params

--- src/detail/user_params.hpp
+++ src/detail/user_params.hpp
@@ -12,4 +12,8 @@ struct user_params_t
   std::string outdir, model_case;
   bool th_src, rv_src, rc_src, rr_src, nc_src, nr_src, uv_src, w_src;
   setup::real_t sgs_delta;
+  quantity<si::length, setup::real_t> mean_rd1, mean_rd2;
+  quantity<si::dimensionless, setup::real_t> sdev_rd1, sdev_rd2;
+  quantity<power_typeof_helper<si::length, static_rational<-3>>::type, setup::real_t> n1_stp, n2_stp;
+  quantity<si::dimensionless, setup::real_t> kappa1, kappa2;
 };
