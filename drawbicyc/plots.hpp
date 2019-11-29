a37a611e907c644c650422cc998cd053b2f0b902
pdziekan <pdziekanp@gmail.com>
2019-11-29 14:45:41 +0100

    add the pr_flux_vs_cl_hgt plot

--- drawbicyc/plots.hpp
+++ drawbicyc/plots.hpp
@@ -88,6 +88,10 @@ std::vector<std::string> profs_rico({
 //, "nc_down" 
 }); // rtot has to be first
 
+std::vector<std::string> profs_base_prflux_vs_clhght({
+"base_prflux_vs_clhght"
+}); 
+
 std::vector<std::string> profs_sgs({
  "sgs_tke"
 ,"k_m"
@@ -161,10 +165,13 @@ class Plots
       series.insert(series.end(), series_moist_thermal.begin(), series_moist_thermal.end());
       fields.insert(fields.end(), fields_moist_thermal.begin(), fields_moist_thermal.end());
     }
+    else if(type == "base_prflux_vs_clhght") { 
+      profs.insert(profs.end(), profs_base_prflux_vs_clhght.begin(), profs_base_prflux_vs_clhght.end());
+    }
     else
       throw std::runtime_error("drawbicyc Plots.hpp: unknown 'type'.");
     
-    if (sgs)
+    if (sgs && type != "base_prflux_vs_clhght")
     {
       profs.insert(profs.end(), profs_sgs.begin(), profs_sgs.end());
       series.insert(series.end(), series_sgs.begin(), series_sgs.end());
