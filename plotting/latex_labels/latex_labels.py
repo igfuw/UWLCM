424ba610656588b8da0ed46954fc3fa13555655e
pdziekan <pdziekanp@gmail.com>
2020-01-08 16:05:34 +0100

    dycoms plotting: series comparison similar to rico series comparison

---
+++ plotting/latex_labels/latex_labels.py
@@ -0,0 +1,21 @@
+var_labels = {
+  "lwp" : 'LWP [g m$^{-2}$]',
+  "rwp" : 'RWP [g m$^{-2}$]',
+  "surf_precip" : 'Surface precip. [mm d$^{-1}$]',
+  "acc_precip" : 'Accumulated precip. [mm]',
+  "cl_nc" : '$N_c$ [cm$^{-3}$] (cloudy cells)',
+  "thl" : r'$\theta_l$ [K]',
+  "00rtot" : '$q_{t}$ [g/kg]',
+  "rliq" : '$q_{l}$ [g/kg]',
+  "clfrac" : 'Cloud fraction',
+  "prflux" : 'Precip. flux [W m$^{-2}$]',
+  "wvar" : r'Var$\left(w\right)$ [m$^2$ s$^{-2}$]',
+  "w3rd" : 'Third mom. of $w$ [m$^3$ s$^{-3}$]',
+  "sat_RH" : 'Supersaturation [\%]',
+  "rad_flx" : 'radiative flux [W m$^{-2}$]',
+  "cl_nc_zoom" : '$N_c$ [cm$^{-3}$]',
+  "base_prflux_vs_clhght" : 'precip flux at cloud base [W m$^{-2}$]',
+  "er" : 'Entrainment rate [cm s$^{-1}$]',
+  "wvarmax" : 'Max. $w$ variance [m$^{2}$ s$^{-2}$]',
+  "cloud_base" : 'Cloud base height [m]'
+}
