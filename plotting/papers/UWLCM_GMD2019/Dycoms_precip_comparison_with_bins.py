0bd042a6dcb769da70ff5a76636bde83ad3e522b
pdziekan <pdziekanp@gmail.com>
2020-01-08 16:16:07 +0100

    surf precip fix

--- plotting/papers/UWLCM_GMD2019/Dycoms_precip_comparison_with_bins.py
+++ plotting/papers/UWLCM_GMD2019/Dycoms_precip_comparison_with_bins.py
@@ -6,8 +6,8 @@ from Dycoms_comparison_common import *
 # activate latex text rendering
 rc('text', usetex=True)
 
-dycoms_profs = ["ql", "precip", "ndrop_cld"]
-dycoms_series = ["lwp", "precip", "ndrop_cld"]
+dycoms_profs = ["rliq", "prflux", "cl_nc"]
+dycoms_series = ["lwp", "surf_precip", "cl_nc"]
 nplots = len(dycoms_profs + dycoms_series)# + 2 # 2 updraft profiles without dycoms results
 
 # init the plot
@@ -16,12 +16,14 @@ nploty = 3 # int(float(nplots)/float(nplotx) + 0.5)
 fig, axarr = plt.subplots(nplotx, nploty )
 
 plot_iter=0
-for var in dycoms_series:
-  print var, plot_iter
-  plot_iter = plot_series(var, plot_iter, nplotx, nploty, axarr, True, suffix="series.dat", xlabel='Time [h]')
-for var in dycoms_profs:
-  print var, plot_iter
-  plot_iter = plot_profiles(var, plot_iter, nplotx, nploty, axarr, True, suffix="profiles_7200_21600.dat", ylabel = '$z/z_i$')
+#for var in dycoms_series:
+#  print var, plot_iter
+#  plot_iter = plot_series(var, plot_iter, nplotx, nploty, axarr, True, suffix="series.dat", xlabel='Time [h]')
+#for var in dycoms_profs:
+#  print var, plot_iter
+#  plot_iter = plot_profiles(var, plot_iter, nplotx, nploty, axarr, True, suffix="profiles_7200_21600.dat", ylabel = '$z/z_i$')
+plot_iter = plot_series(dycoms_series, plot_iter, nplotx, nploty, axarr, True, suffix="series.dat", xlabel='Time [h]')
+plot_iter = plot_profiles(dycoms_profs, plot_iter, nplotx, nploty, axarr, True, suffix="profiles_7200_21600.dat", ylabel = '$z/z_i$')
 
 
 # legend font size
