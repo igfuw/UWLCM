6f6e3a9b8752d38e81f555b4f645b672cf4e0afe
pdziekan <pdziekanp@gmail.com>
2020-01-10 13:54:29 +0100

    GCCN plotting: rico precip fixes

--- plotting/Matplotlib_common/plot_profs.py
+++ plotting/Matplotlib_common/plot_profs.py
@@ -22,13 +22,15 @@ def plot_profiles(var_list, plot_iter, nplotx, nploty, axarr, xscaledict, yscale
       profiles_file = open(file_name, "r")
       my_pos = read_my_var(profiles_file, "position")
       my_res = read_my_var(profiles_file, var)
-   
-   #     print 'mean nc in cloud cells: ' , np.mean(my_nc[my_nc>20])
-    
       profiles_file.close()
+
+      # remove artificial values of cloudy-cell variables near the surface due to incorrect cloudiness mask (e.g. RICO mask used in DYCOMS)
+      if(var == "gccn_rw_cl" or var == "non_gccn_rw_cl" or var == "cl_nc"):
+        my_res[0:3] = 0
     
       linestyles = ['--', '-.', ':']
       dashList = [(3,1),(1,1),(4,1,1,1),(4,2)] 
+
       if(var == "base_prflux_vs_clhght"):
         plot_my_array(axarr, plot_iter, my_res, my_pos, nploty, xlabel=var_labels[var], ylabel="cloudy column height [m]", varlabel=profiles_labels[label_counter], dashes = dashList[label_counter % len(dashList)], xscale=xscaledict[var], yscale=yscaledict[var], xlim=xlimdict[var], ylim=ylimdict[var])
       else:
