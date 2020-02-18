25487fec1fbf1c21d09cbe7b53a66d6c51a3937c
pdziekan <pdziekanp@gmail.com>
2020-02-18 12:38:53 +0100

    plotting: side-by-side comparison of baseprflux vs column height

--- plotting/Matplotlib_common/plot_profs.py
+++ plotting/Matplotlib_common/plot_profs.py
@@ -4,19 +4,18 @@ from sys import argv
 from latex_labels import var_labels
 from read_UWLCM_arrays import *
 
-def plot_profiles(var_list, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict, ylimdict, suffix='', ylabel=''):
-  #read my results
-  profiles_files_names = []
-  profiles_labels = []
-  file_no = np.arange(1, len(argv)-1 , 2)
-  print file_no
-  for no in file_no:
-    profiles_files_names.append(argv[no]+suffix)
-    profiles_labels.append(argv[no+1])
+def plot_profiles(var_list, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict, ylimdict, suffix='', ylabel='', file_names=[], file_labels=[]):
+  # if file names are not defined, read them and labels from command line
+  if len(file_names)==0:
+    file_no = np.arange(1, len(argv)-1 , 2)
+    print file_no
+    for no in file_no:
+      file_names.append(argv[no]+suffix)
+      file_labels.append(argv[no+1])
   
   for var in var_list:
     label_counter = 0
-    for file_name in profiles_files_names:
+    for file_name in file_names:
       print file_name
     #  try:
       profiles_file = open(file_name, "r")
@@ -32,9 +31,9 @@ def plot_profiles(var_list, plot_iter, nplotx, nploty, axarr, xscaledict, yscale
       dashList = [(3,1),(1,1),(4,1,1,1),(4,2)] 
 
       if(var == "base_prflux_vs_clhght" or var == "base_prflux_vs_clhght number of occurances"):
-        plot_my_array(axarr, plot_iter, my_res, my_pos, nploty, xlabel=var_labels[var], ylabel="cloudy column height [m]", varlabel=profiles_labels[label_counter], dashes = dashList[label_counter % len(dashList)], xscale=xscaledict[var], yscale=yscaledict[var], xlim=xlimdict[var], ylim=ylimdict[var])
+        plot_my_array(axarr, plot_iter, my_res, my_pos, nploty, xlabel=var_labels[var], ylabel="cloudy column height [m]", varlabel=file_labels[label_counter], dashes = dashList[label_counter % len(dashList)], xscale=xscaledict[var], yscale=yscaledict[var], xlim=xlimdict[var], ylim=ylimdict[var])
       else:
-        plot_my_array(axarr, plot_iter, my_res, my_pos, nploty, xlabel=var_labels[var], ylabel=ylabel, varlabel=profiles_labels[label_counter], dashes = dashList[label_counter % len(dashList)], xscale=xscaledict[var], yscale=yscaledict[var], xlim=xlimdict[var], ylim=ylimdict[var])
+        plot_my_array(axarr, plot_iter, my_res, my_pos, nploty, xlabel=var_labels[var], ylabel=ylabel, varlabel=file_labels[label_counter], dashes = dashList[label_counter % len(dashList)], xscale=xscaledict[var], yscale=yscaledict[var], xlim=xlimdict[var], ylim=ylimdict[var])
    #   except:
    #     print 'error opening file: ', file_name
    #     my_pos = 0
--- plotting/papers/GCCN_LES/results_vs_CCN.py
+++ plotting/papers/GCCN_LES/results_vs_CCN.py
@@ -5,7 +5,8 @@ from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Matplotlib_common/")
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Dycoms_RF02/")
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../Dycoms_RF02/")
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../RICO11/")
 
 from plot_ranges import * 
 from plot_series import *
@@ -14,7 +15,8 @@ from plot_profs import *
 # activate latex text rendering
 rc('text', usetex=True)
 
-series = ["surf_precip"]
+#series = ["surf_precip"]
+profs = ["base_prflux_vs_clhght"]
 nplotx = 1
 nploty = 3
 
@@ -33,12 +35,12 @@ for ccn_iter in [0,1,2]: # clean, standard, polluted
   for no in file_no:
     print no
     print argv[no]
-    file_names.append(argv[no] + "series.dat")
+    file_names.append(argv[no] + "profiles_18000_36000.dat")
     file_labels.append(argv[no+1])
   
   print file_names
-  plot_iter = plot_series(series, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict_series, ylimdict_series, xlabel='Time [h]', file_names=file_names, file_labels=file_labels)
-#plot_profiles(dycoms_profs, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict_profs, ylimdict_profs, suffix="profiles_7200_18000.dat", ylabel='$z/z_i$')
+#  plot_iter = plot_series(series, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict_series, ylimdict_series, xlabel='Time [h]', file_names=file_names, file_labels=file_labels)
+  plot_iter = plot_profiles(profs, plot_iter, nplotx, nploty, axarr, xscaledict, yscaledict, xlimdict_profs, ylimdict_profs, ylabel='$z/z_i$', file_names=file_names, file_labels=file_labels)
 
 # legend font size
 plt.rcParams.update({'font.size': 8})
