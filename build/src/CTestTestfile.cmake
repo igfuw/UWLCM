a15e03d1d02847ef83af0213f0af02dbd1a1a2ed
ryba183 <pz405506@icm.edu.pl>
2020-02-21 13:51:00 +0100

    dont trakc build and git_revision

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pzmij/biblioteki/UWLCM/src
-# Build directory: /home/pzmij/biblioteki/UWLCM/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pzmij/biblioteki/UWLCM/tests
-# Build directory: /home/pzmij/biblioteki/UWLCM/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("unit")
-subdirs("moist_thermal")
