1c11b6ef40bcb4487d302c16f48dd60b87409179
pdziekan <pdziekanp@gmail.com>
2020-03-24 16:48:02 +0100

    tests: use installed uwlcm

--- tests/moist_thermal/moist_thermal.cpp
+++ tests/moist_thermal/moist_thermal.cpp
@@ -258,7 +258,7 @@ int main(int ac, char** av)
   {
     // run the simulation
     ostringstream cmd;
-    cmd << av[1] << "/src/bicycles " << opts_common << " " << opts_m.second;
+    cmd << "uwlcm " << opts_common << " " << opts_m.second;
     notice_macro("about to call: " << cmd.str())
 
     if (EXIT_SUCCESS != system(cmd.str().c_str()))
