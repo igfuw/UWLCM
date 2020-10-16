070fcace5b0404e4bd0c7dc2c98e8e17399f3342
pdziekan <pdziekanp@gmail.com>
2020-10-16 10:36:05 +0200

    fix include order 2

--- src/run_hlpr.cpp
+++ src/run_hlpr.cpp
@@ -17,23 +17,24 @@
 #include "cases/DryThermalGMD2015.hpp"
 #include "cases/LasherTrapp2001.hpp"
 
+#include "opts/opts_common.hpp"
 #include "forcings/calc_forces_common.hpp"
 
 #if !defined(UWLCM_DISABLE_2D_LGRNGN) &&  !defined(UWLCM_DISABLE_3D_LGRNGN)
-  #include "solvers/slvr_lgrngn.hpp"
   #include "opts/opts_lgrngn.hpp"
+  #include "solvers/slvr_lgrngn.hpp"
 #endif
 
 #if !defined(UWLCM_DISABLE_2D_BLK_1M) &&  !defined(UWLCM_DISABLE_3D_BLK_1M)
+  #include "opts/opts_blk_1m.hpp"
   #include "solvers/slvr_blk_1m.hpp"
   #include "forcings/calc_forces_blk_1m.hpp"
-  #include "opts/opts_blk_1m.hpp"
 #endif
 
 #if !defined(UWLCM_DISABLE_2D_BLK_2M) &&  !defined(UWLCM_DISABLE_3D_BLK_2M)
+  #include "opts/opts_blk_2m.hpp"
   #include "solvers/slvr_blk_2m.hpp"
   #include "forcings/calc_forces_blk_2m.hpp"
-  #include "opts/opts_blk_2m.hpp"
 #endif
 
 #include "run_hlpr.hpp"
