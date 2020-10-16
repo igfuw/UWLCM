ba63862e5cff010e4f5f04d8bd2b660f54dcd038
pdziekan <pdziekanp@gmail.com>
2020-10-16 12:36:08 +0200

    fix treatment of DISABLE_

--- src/uwlcm.cpp
+++ src/uwlcm.cpp
@@ -18,16 +18,16 @@
 
 #include "detail/ct_params.hpp"
 
-#if !defined(UWLCM_DISABLE_2D_LGRNGN) &&  !defined(UWLCM_DISABLE_3D_LGRNGN)
+#if !defined(UWLCM_DISABLE_2D_LGRNGN) || !defined(UWLCM_DISABLE_3D_LGRNGN)
   #include "solvers/slvr_lgrngn.hpp"
 #endif
 
-#if !defined(UWLCM_DISABLE_2D_BLK_1M) &&  !defined(UWLCM_DISABLE_3D_BLK_1M)
+#if !defined(UWLCM_DISABLE_2D_BLK_1M) || !defined(UWLCM_DISABLE_3D_BLK_1M)
   #include "solvers/slvr_blk_1m.hpp"
   #include "forcings/calc_forces_blk_1m.hpp"
 #endif
 
-#if !defined(UWLCM_DISABLE_2D_BLK_2M) &&  !defined(UWLCM_DISABLE_3D_BLK_2M)
+#if !defined(UWLCM_DISABLE_2D_BLK_2M) || !defined(UWLCM_DISABLE_3D_BLK_2M)
   #include "solvers/slvr_blk_2m.hpp"
   #include "forcings/calc_forces_blk_2m.hpp"
 #endif
