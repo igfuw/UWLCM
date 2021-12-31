9988dc21672f80234b4a898f5a8112e684b49eb2
pdziekan <pdziekanp@gmail.com>
2021-12-31 13:52:06 +0100

    slvr sgs: fix timer name only with timing

--- src/solvers/slvr_sgs.hpp
+++ src/solvers/slvr_sgs.hpp
@@ -11,7 +11,9 @@ class slvr_sgs : public slvr_common<ct_params_t>
   public:
   using real_t = typename ct_params_t::real_t;
   using ix = typename ct_params_t::ix;
+#if defined(UWLCM_TIMING)
   using parent_t::timer;
+#endif
 
   protected:
 
