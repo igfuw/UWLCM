e2430d7eeac6361aea991c973624ebc0dc70678d
pdziekan <pdziekanp@gmail.com>
2022-01-14 12:16:37 +0100

    setting max number of sd: make space for src_sd_conc only if src used and take into account src_z1

--- src/solvers/lgrngn/hook_ante_loop_lgrngn.hpp
+++ src/solvers/lgrngn/hook_ante_loop_lgrngn.hpp
@@ -40,8 +40,11 @@ void slvr_lgrngn<ct_params_t>::hook_ante_loop(int nt)
     for (auto const& krcm : params.cloudph_opts_init.dry_sizes)
       for (auto const& rcm : krcm.second)
         n_sd_from_dry_sizes += rcm.second.second;
+
+    // src_dry_distros is used only in the first step to add GCCN below some level
+    int n_sd_from_src_dry_distros = params.cloudph_opts_init.src_sd_conc * params.cloudph_opts_init.src_z1 / params.cloudph_opts_init.z1 + 0.5;
       
-    const int n_sd_per_cell = params.cloudph_opts_init.sd_conc + n_sd_from_dry_sizes + 40; // +40 temporary to account for GCCN that are added via souce, not init dry sizes
+    const int n_sd_per_cell = params.cloudph_opts_init.sd_conc + n_sd_from_dry_sizes + n_sd_from_src_dry_distros;
 
     if(parent_t::n_dims == 2) // 2D
     {
