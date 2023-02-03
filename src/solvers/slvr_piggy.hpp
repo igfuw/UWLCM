334a970de27f784220d91314cd7c49e87a68fecb
Piotr Dziekan <pdziekanp@gmail.com>
2023-02-03 15:46:15 +0100

    hdf5 velocity output, nohalo

--- src/solvers/slvr_piggy.hpp
+++ src/solvers/slvr_piggy.hpp
@@ -31,17 +31,20 @@ class slvr_piggy<
   using parent_t = output::hdf5_xdmf<
     solvers::mpdata_rhs_vip_prs_sgs<ct_params_t, minhalo>
   >;  
+
   std::unique_ptr<H5::H5File> hdfpu;
+  std::map<int, H5::DataSet> vels;
+
   const std::string vel_out_name = "velocity_out.h5";
   std::ofstream f_vel_out; // file for velocity field
   
-  const H5::FloatType
-	  flttype_output = H5::PredType::NATIVE_FLOAT;
-
-  hid_t fapl_id;
-  blitz::TinyVector<hsize_t, parent_t::n_dims> cshape, shape, chunk, srfcshape, srfcchunk, offst;
-  H5::DSetCreatPropList params;
-  H5::DataSpace sspace, cspace, srfcspace;
+//  const H5::FloatType
+//    flttype_output = H5::PredType::NATIVE_FLOAT;
+//
+//  hid_t fapl_id;
+//  blitz::TinyVector<hsize_t, parent_t::n_dims> cshape, shape, chunk, srfcshape, srfcchunk, offst;
+//  H5::DSetCreatPropList params;
+//  H5::DataSpace sspace, cspace, srfcspace;
 
   void hook_ante_loop(int nt) 
   {
@@ -51,20 +54,24 @@ class slvr_piggy<
       // open file for out vel
       if(save_vel)
       {
-	hdfpu.reset(new H5::H5File(this->outdir + "/" + vel_out_name, H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id));
-        std::map<int, H5::DataSet> vars;
+        hdfpu.reset(new H5::H5File(this->outdir + "/" + vel_out_name, H5F_ACC_TRUNC
+#if defined(USE_MPI)
+          , H5P_DEFAULT, this->fapl_id
+#endif
+        ));
+
        
         for (int d = 0; d < parent_t::n_dims; ++d)
         {
           // creating the user-requested variables
-          vars[d] = (*hdfpu).createDataSet(
+          vels[d] = (*hdfpu).createDataSet(
             this->outvars[this->vip_ixs[d]].name,
- 	    flttype_output,
-            sspace,
-            params
+            this->flttype_output,
+            this->sspace,
+            this->params
           );
-	}
-	try{
+        }
+        try{
           f_vel_out.open(this->outdir+"/velocity_out.dat"); 
         }
         catch(...)
@@ -87,8 +94,10 @@ class slvr_piggy<
     {
       for (int d = 0; d < parent_t::n_dims; ++d)
       {
-	//vars.write(vars[d], this->state(this->vip_ixs[d]));
-	f_vel_out << this->state(this->vip_ixs[d]);
+        //vars.write(vars[d], this->state(this->vip_ixs[d]));
+        this->record_dsc_helper(vels[d], this->state(this->vip_ixs[d]));
+
+        f_vel_out << this->state(this->vip_ixs[d]);
       }
     }
   }
