ea099a0d7eadf47480611207f988141efdd7dd96
ryba183 <ryba183@gmail.com>
2023-02-05 16:20:40 +0100

    saving velocity_out in every timestep (commented), now velocity_out.h5 consists only of the u/w/v values for the last time step, the previous one are not saved.

--- src/solvers/slvr_piggy.hpp
+++ src/solvers/slvr_piggy.hpp
@@ -48,6 +48,19 @@ class slvr_piggy<
   H5::DSetCreatPropList params_h;
   H5::DataSpace sspace_h;
 
+  std::string base_name()
+  {
+    std::stringstream ss;
+    ss << "velocity_out" << std::setw(10) << std::setfill('0') << this->timestep;
+    return ss.str();
+  }
+
+  std::string hdf_name()
+  {
+    // TODO: add option of .nc extension for Paraview sake ?
+    return base_name() + ".h5";
+  }
+
   void hook_ante_loop(int nt) 
   {
     parent_t::hook_ante_loop(nt); 
@@ -64,16 +77,16 @@ class slvr_piggy<
 
         for (int d = 0; d < parent_t::n_dims; ++d)
           shape_h[d] = this->mem->distmem.grid_size[d] + 2*this->halo;
-
+        // shape_h[parent_t::n_dims] = nt; //make parent_t::n_dims+1 to work
 
         offst_h = 0;       // TODO: fix for MPI
         chunk_h = shape_h; // TODO: this wont work for MPI
+        // chunk_h[parent_t::n_dims] = 1;
         sspace_h = H5::DataSpace(parent_t::n_dims, shape_h.data());
         params_h.setChunk(parent_t::n_dims, chunk_h.data());
-        // TODO: for MPI set deflate
 
 
-       
+        // TODO: for MPI set deflate      
         for (int d = 0; d < parent_t::n_dims; ++d)
         {
           // creating the user-requested variables
@@ -84,7 +97,7 @@ class slvr_piggy<
             params_h
           );
         }
-        try{
+      try{
           f_vel_out.open(this->outdir+"/velocity_out.dat"); 
         }
         catch(...)
@@ -105,16 +118,39 @@ class slvr_piggy<
     // save velocity field
     if(this->rank==0 && save_vel)
     {
+//       hdfpu.reset(new H5::H5File(this->outdir + "/" + hdf_name(), H5F_ACC_TRUNC
+// #if defined(USE_MPI)
+//           , H5P_DEFAULT, this->fapl_id
+// #endif
+//         ));
+
+//         for (int d = 0; d < parent_t::n_dims; ++d)
+//           shape_h[d] = this->mem->distmem.grid_size[d] + 2*this->halo;
+//         shape_h[parent_t::n_dims] = 10;
+
+//         offst_h = 0;       // TODO: fix for MPI
+//         std::cout<<"shape_h size "<<shape_h;
+//         chunk_h = shape_h; // TODO: this wont work for MPI
+//         chunk_h[parent_t::n_dims] = 1;
+//         sspace_h = H5::DataSpace(parent_t::n_dims+1, shape_h.data());
+//         params_h.setChunk(parent_t::n_dims+1, chunk_h.data());
+//         // TODO: for MPI set deflate      
+//         for (int d = 0; d < parent_t::n_dims; ++d)
+//         {
+//           // creating the user-requested variables
+//           vels[d] = (*hdfpu).createDataSet(
+//             this->outvars[this->vip_ixs[d]].name,
+//             this->flttype_output,
+//             sspace_h,
+//             params_h
+//           );
+//         }
+
       for (int d = 0; d < parent_t::n_dims; ++d)
       {
-        //vars.write(vars[d], this->state(this->vip_ixs[d]));
-        //this->record_dsc_helper(vels[d], this->state(this->vip_ixs[d]));
-
         auto space = vels[d].getSpace();
         space.selectHyperslab(H5S_SELECT_SET, shape_h.data(), offst_h.data());
         vels[d].write(this->state(this->vip_ixs[d]).data(), this->flttype_solver, H5::DataSpace(parent_t::n_dims, shape_h.data()), space, this->dxpl_id);
-
-
         f_vel_out << this->state(this->vip_ixs[d]);
       }
     }
