21225890b7c3130ad742a139ba88a2948d81ecb2
Piotr Dziekan <plgpdziekan@login01.athena.cyfronet.pl>
2023-04-07 12:14:10 +0200

    replace direct calls to spatial_average_ref2reg with this->spatial_average_ref2reg

--- src/solvers/lgrngn/hook_ante_delayed_step_lgrngn.hpp
+++ src/solvers/lgrngn/hook_ante_delayed_step_lgrngn.hpp
@@ -51,18 +51,15 @@ void slvr_lgrngn<ct_params_t>::hook_ante_delayed_step()
 //    std::cerr << "th diff cond: " << th_post_cond(this->ijk_ref) << std::endl;
 
     this->mem->barrier();
-    libmpdataxx::formulae::refined::spatial_average_ref2reg<real_t>(th_post_cond, this->ijk_r2r, this->mem->n_ref/2, this->mem->distmem.grid_size_ref, true);
-    libmpdataxx::formulae::refined::spatial_average_ref2reg<real_t>(rv_post_cond, this->ijk_r2r, this->mem->n_ref/2, this->mem->distmem.grid_size_ref, true);
-
-    dth(this->ijk) = th_post_cond(this->ijk_r2r);
-    drv(this->ijk) = rv_post_cond(this->ijk_r2r);
+    this->spatial_average_ref2reg(th_post_cond, dth);
+    this->spatial_average_ref2reg(rv_post_cond, drv);
 
  //   std::cerr << "dth: " << dth(this->ijk) << std::endl;
  //   std::cerr << "drv: " << drv(this->ijk) << std::endl;
 
     // with cyclic bcond, th and rv in corresponding edge cells needs to change by the same amount
-    this->avg_edge_sclr(dth, this->ijk);
-    this->avg_edge_sclr(drv, this->ijk);
+//    this->avg_edge_sclr(dth, this->ijk);
+//    this->avg_edge_sclr(drv, this->ijk);
 
     this->state(ix::th)(this->ijk) += dth(this->ijk); 
     this->state(ix::rv)(this->ijk) += drv(this->ijk); 
